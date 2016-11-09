1120223317a79d483f5cab5ee979e5374c4d3543
Christoph Matthies <christoph.matthies@gmail.com>
2016-11-09 16:39:27 +0100

    Fix(Seeds): Refactor seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,55 +1,70 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
 
-users = [
-    {name: "Lazy Student", email: "lazy.student@example.com"},
-    {name: "Willi Workshoprequester", email: "willi@example.com"},
-    {name: "Arthur Applicant", email: "arthur@example.com"}
-]
+# Users
+users = []
 
+pupil = User.find_or_initialize_by(
+    name: "Schueler",
+    email: "schueler@example.com"
+)
+users.push(pupil)
+
+teacher = User.find_or_initialize_by(
+    name: "Lehrer",
+    email: "lehrer@example.com"
+)
+users.push(teacher)
+
+applicant = User.find_or_initialize_by(
+    name: "Bewerber",
+    email: "bewerber@example.com"
+)
+users.push(applicant)
+
+# Set a password for every user
+# They are only initialized, save them to the db
 users.each do |user|
-  initialized_user = User.find_or_initialize_by(
-          name: user[:name],
-          email: user[:email],
-  )
-  initialized_user.password = "test12345"
-  initialized_user.save!
+  user.password = "123456"
+  user.save!
 end
 
-workshop_name = "Our first Workshop"
-Workshop.find_or_create_by!(
-    name: workshop_name,
-    description: "The first workshop, we created",
+# A workshop
+workshop = Workshop.find_or_create_by!(
+    name: "Messung und Verarbeitung von Umweltdaten",
+    description: "Workshop mit Phidgets und Etoys",
     max_participants: 20,
     active: true
 )
 
+# Pupil's profile
 Profile.find_or_create_by!(
-    cv: "I am lazy, so I did not apply to any Workshop or requested anything",
-    user: User.find_by(email: users[0][:email])
+    cv: "Ich habe mich zu keinem Workshop beworben",
+    user: pupil
 )
 
+# Teacher's profile
+Profile.find_or_create_by!(
+    cv: "Ich bin ein Lehrer. Ich frage Workshops für meine Schüler an.",
+    user: teacher
+)
+
+# Applicant's profile
+Profile.find_or_create_by!(
+    cv: "Ich bin ein Schüler, der an Workshops teilnehmen möchte.",
+    user: applicant
+)
+
+# Teacher's workshop request
 Request.find_or_create_by!(
-    topics: "Ruby on Rails Programming",
-    user: User.find_by(email: users[1][:email])
+    topics: "Hardware-Entwicklung mit einem CAD-System",
+    user: teacher
 )
 
-Profile.find_or_create_by!(
-    cv: "I have started with Rails and like to improve my knowledge in workshops. Would be cool if somebody could organize a Workshop",
-    user: User.find_by(email: users[1][:email])
-)
-
+# Applicant's application letter
 ApplicationLetter.find_or_create_by!(
-    motivation: "I would really like to take part in the first workshop.",
-    user: User.find_by(email: users[2][:email]),
-    workshop: Workshop.find_by(name: workshop_name)
-)
-Profile.find_or_create_by!(
-    cv: "No experience with workshops yet, so let me join.",
-    user: User.find_by(email: users[2][:email])
+    motivation: "Ich würde sehr gerne an eurem Workshop teilnehmen",
+    user: applicant,
+    workshop: workshop
 )
