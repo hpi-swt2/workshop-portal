5e727e708e15697d0a4f43d7bba2796392d3f667
alexanderernst <leverkusener77@googlemail.com>
2016-11-09 14:42:41 +0100

    added seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,50 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+if Workshop.where(name: "Our first Workshop").empty?
+  Workshop.create!(
+              name: "Our first Workshop",
+              description: "The first workshop, we created",
+              max_participants: 20,
+              active: true
+  )
+end
+
+if User.where(email: "marko@mustermann.de").empty?
+  Profile.create!(
+      cv: "A very impressive CV",
+      user: User.create!(
+          name: "Marko Mustermann",
+          email: "marko@mustermann.de",
+          password: "test12345"
+      )
+  )
+end
+
+if Request.where(topics: "Ruby on Rails Programming").empty?
+  Request.create!(
+      topics: "Ruby on Rails Programming",
+      user: User.create!(
+          name: "Mario Mustermann",
+          email: "mario@mustermann.de",
+          password: "test12345"
+      )
+  )
+  Profile.create!(
+      cv: "I have started with Rails and like to improve my knowledge in workshops.",
+      user: User.find_by_email("mario@mustermann.de")
+  )
+end
+
+if ApplicationLetter.where(motivation: "I would really like to take part at the first workshop.").empty?
+  ApplicationLetter.create!(
+      motivation: "I would really like to take part at the first workshop.",
+      user: User.create!(
+          name: "Mirko Mustermann",
+          email: "mirko@mustermann.de",
+          password: "test12345"
+      ),
+      workshop: Workshop.find_by_name("Our first Workshop")
+  )
+  Profile.create!(
+      cv: "No experience with workshops yet, so let me join.",
+      user: User.find_by_email("mirko@mustermann.de")
+  )
+end
