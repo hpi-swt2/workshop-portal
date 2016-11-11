79793f5ce24cec906b7ae1d44842d27da29277c7
alexanderernst <leverkusener77@googlemail.com>
2016-11-11 11:01:27 +0100

    only use production environment for errbit

--- config/initializers/errbit.rb
+++ config/initializers/errbit.rb
@@ -2,4 +2,5 @@ Airbrake.configure do |config|
   config.host = 'https://swt2-errbit-2016.herokuapp.com'
   config.project_id = 1
   config.project_key = '7cdd58bf325c8a3c1ddb90b34566c94b'
+  config.ignore_environments = %w(development test cucumber)
 end
