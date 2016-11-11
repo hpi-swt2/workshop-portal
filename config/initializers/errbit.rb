76c8a6ac478271a83004797f7099c1898cf5b1ee
alexanderernst <leverkusener77@googlemail.com>
2016-11-11 11:27:42 +0100

    only log production to errbit

--- config/initializers/errbit.rb
+++ config/initializers/errbit.rb
@@ -2,5 +2,6 @@ Airbrake.configure do |config|
   config.host = 'https://swt2-errbit-2016.herokuapp.com'
   config.project_id = 1
   config.project_key = '7cdd58bf325c8a3c1ddb90b34566c94b'
+  config.environment = Rails.env
   config.ignore_environments = %w(development test cucumber)
 end
