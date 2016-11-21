24be7cbf1c7ba0a85687e2491b36907f755b8418
TimBasel <tim.basel@student.hpi.de>
2016-11-21 11:33:16 +0100

    Revert "Revert "removed intentional bug""
    This reverts commit 71f77b87e3c0b3797a15f5941752eceedce1613d.

--- spec/requests/workshops_spec.rb
+++ spec/requests/workshops_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe "Workshops", type: :request do
   describe "GET /workshops" do
-    xit "works! (now write some real specs)" do
+    it "works! (now write some real specs)" do
       get workshops_path
       expect(response).to have_http_status(200)
     end
