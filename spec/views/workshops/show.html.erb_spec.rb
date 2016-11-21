36450be3c232bb48da53eb4e35041efa26a4797b
TimBasel <tim.basel@student.hpi.de>
2016-11-21 11:35:11 +0100

    Revert "Failing Tests for Workshop Overview Page (#23"
    This reverts commit 8ecf907ff43f8bafeaab4923243c3cced038e7dc.
    
    Sorry, it might be a good idea to lock the master branch for direct commits

--- spec/views/workshops/show.html.erb_spec.rb
+++ spec/views/workshops/show.html.erb_spec.rb
@@ -11,29 +11,4 @@ RSpec.describe "workshops/show", type: :view do
     expect(rendered).to have_text(@workshop.description)
     expect(rendered).to have_text(@workshop.max_participants)
   end
-
-  it "renders applicants table" do
-    render
-    expect(rendered).to have_table("applicants")
-  end
-
-  it "renders applicants table header fields" do
-    render
-    expect(rendered).to have_css("th", :text => "Name")
-    expect(rendered).to have_css("th", :text => "Geschlecht")
-    expect(rendered).to have_css("th", :text => "Alter")
-    expect(rendered).to have_css("th", :text => "Camp-Teilnahmen")
-    expect(rendered).to have_css("th", :text => "Status")
-  end
-
-  it "displays applicants information" do
-    @application_letter = FactoryGirl.create(:application_letter, user: FactoryGirl.create(:user), workshop: @workshop)
-    @workshop.application_letters.push(@application_letter)
-
-    render
-    expect(rendered).to have_css("td", :text => @application_letter.user.name)
-  end
-
-
-
 end
