acdaa77836cbacccf13df5383fb91b08863cab4e
TimBasel <tim.basel@student.hpi.de>
2016-11-21 11:54:14 +0100

    Failing Tests for Workshop Overview Page (#23

--- spec/views/workshops/show.html.erb_spec.rb
+++ spec/views/workshops/show.html.erb_spec.rb
@@ -3,7 +3,11 @@ require 'rails_helper'
 RSpec.describe "workshops/show", type: :view do
   before(:each) do
     @workshop = assign(:workshop, FactoryGirl.create(:workshop))
+    @application_letter = FactoryGirl.create(:application_letter, user: FactoryGirl.create(:user), workshop: @workshop)
+    @workshop.application_letters.push(@application_letter)
   end
+
+
 
   it "renders attributes" do
     render
@@ -11,4 +15,29 @@ RSpec.describe "workshops/show", type: :view do
     expect(rendered).to have_text(@workshop.description)
     expect(rendered).to have_text(@workshop.max_participants)
   end
+
+  it "renders applicants table" do
+    render
+    expect(rendered).to have_table("applicants")
+  end
+
+  it "renders applicants table header fields" do
+    render
+    expect(rendered).to have_css("th", :text => "Name")
+    expect(rendered).to have_css("th", :text => "Geschlecht")
+    expect(rendered).to have_css("th", :text => "Alter")
+    expect(rendered).to have_css("th", :text => "Camp-Teilnahmen")
+    expect(rendered).to have_css("th", :text => "Status")
+  end
+
+  it "displays applicants information" do
+    render
+    expect(rendered).to have_css("td", :text => @workshop.application_letters.first.user.name)
+  end
+
+  it "displays application details button" do
+    render
+    expect(rendered).to have_link("Details")
+  end
+
 end
