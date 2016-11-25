c1b6e8dcd59f6cc2d379ce840a7b7a7ec1160a44
Tom Beckmann <tomjonabc@gmail.com>
2016-11-25 13:58:57 +0100

    Adapt tests to work on page output

---
+++ app/assets/javascripts/events.js
@@ -0,0 +1,19 @@
+// Place all the behaviors and hooks related to the matching controller here.
+// All this logic will automatically be available in application.js.
+
+$(function() {
+  $('#event-add-date-picker').click(function() {
+    var template = EVENT_DATE_PICKER_TEMPLATE;
+
+    // insert our template to the ui and add a remove button
+    $(template)
+      .insertBefore(this)
+      .append(' <a style="float: none" class="close">&times;</a>')
+      .find('.close')
+      .click(function() {
+        // remove our template and decrement our count field
+        $(this).parent('div').remove();
+      });
+  });
+});
+
--- spec/features/event_spec.rb
+++ spec/features/event_spec.rb
@@ -2,42 +2,48 @@ require "rails_helper"
 
 describe "Event creation", :type => :feature do
   it "should not allow dates in the past" do
-    visit new_workshop_path
-    fill_in 'event_date_range_start_0', with: '11-11-1997'
-    fill_in 'event_date_range_end_0', with: '11-12-1997'
-    click_button "Workshop Erstellen"
+    visit new_event_path
+    select_date_within_selector(Date.yesterday.prev_day, '.event-date-picker-start')
+    select_date_within_selector(Date.yesterday, '.event-date-picker-end')
+    click_button "Event erstellen"
     expect(page).to have_text("Die Zeitspanne darf nicht in der Vergangenheit liegen")
   end
 
   it "should not allow unreasonable time spans" do
-    visit new_workshop_path
-    fill_in 'event_date_range_start_0', with: '11-11-1997'
-    fill_in 'event_date_range_end_0', with: '11-12-2007'
-    click_button "Workshop Erstellen"
+    visit new_event_path
+    select_date_within_selector(Date.today, '.event-date-picker-start')
+    select_date_within_selector(Date.today.next_year(3), '.event-date-picker-end')
+    click_button "Event erstellen"
     expect(page).to have_text("Die angegebene Zeitspanne umfasst einen ungewöhnlich langen Zeitraum")
   end
 
   it "should allow entering multiple time spans" do
-    visit new_workshop_path
-    fill_in 'event_date_range_start_0', with: '11-11-1997'
-    fill_in 'event_date_range_end_0', with: '11-12-1997'
-    click_button "Zeitspanne hinzufügen"
-    fill_in 'event_date_range_start_1', with: '11-15-1997'
-    fill_in 'event_date_range_end_1', with: '11-16-1997'
-    click_button "Workshop Erstellen"
+    visit new_event_path
 
-    workshop = Workshop.first
-    expect(workshop.date_ranges.first.start).to eq('11-11-1997')
-    expect(workshop.date_ranges.first.end).to eq('11-12-1997')
-    expect(workshop.date_ranges.second.start).to eq('11-15-1997')
-    expect(workshop.date_ranges.second.end).to eq('11-16-1997')
+    first_from = Date.today
+    first_to = Date.today.next_day(2)
+
+    second_from = Date.today.next_day(6)
+    second_to = Date.today.next_day(8)
+
+    select_date_within_selector(first_from, '.event-date-picker-start')
+    select_date_within_selector(first_to, '.event-date-picker-end')
+    page.find("#event-add-date-picker").click
+
+    start = page.all('.event-date-picker')[1].find('.event-date-picker-start')
+    select_date_within_selector(second_from, start)
+    select_date_within_selector(second_to, '.event-date-picker:nth-child(2) .event-date-picker-end')
+    click_button "Event erstellen"
+
+    expect(page).to have_text(first_from.to_s + ' bis ' + first_to.to_s)
+    expect(page).to have_text(second_from.to_s + ' bis ' + second_to.to_s)
   end
 
   it "should not allow an end date before a start date" do
-    visit new_workshop_path
-    fill_in 'event_date_range_start_0', with: '11-11-1997'
-    fill_in 'event_date_range_end_0', with: '11-10-1997'
-    click_button "Workshop Erstellen"
+    visit new_event_path
+    select_date_within_selector(Date.today, '.event-date-picker-start')
+    select_date_within_selector(Date.today.prev_day(-2), '.event-date-picker-end')
+    click_button "Event erstellen"
 
     expect(page).to have_text("End-Datum kann nicht vor Start-Datum liegen")
   end
