5bd696b55427040a153b6e26bbcaa0a9f0ff68b1
Kim Borchart <Kim-Pascal.Borchart@student.hpi.de>
2016-11-25 13:20:00 +0100

    add failing traits for date_ranges factory. Add date_range validation tests, add event test for event without ranges

--- spec/models/date_range_spec.rb
+++ spec/models/date_range_spec.rb
@@ -1,9 +1,20 @@
 require 'rails_helper'
 
 describe DateRange do
-	it "belongs to an Event" do
-		event = create(:event)  #FIXME this will later need to be renamed create(:event)
-		visit event_path(event)
-		expect(event.date_range.size).to eq(1)
-	end
+  let(:event) {FactoryGirl.create :event }
+  let(:dateRange) { FactoryGirl.create :dateRange, event_id: event.id }
+
+	it "has a foreign key for an Event" do
+		expect(dateRange.event_id).to eq(event.id)
+  end
+
+  it "does not validate with dateRanges in the past" do
+    pastDates = build(:dateRange, :with_past_dates)
+    expect(pastDates).to_not be_valid
+  end
+
+  it "should not have a negative range" do
+		negativeDates = build(:dateRange, :with_negative_range)
+    expect(negativeDates.to_not be_valid)
+  end
 end
