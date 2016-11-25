5df6619b753a03a1c37aea88b495194adf7d03ca
Kim Borchart <Kim-Pascal.Borchart@student.hpi.de>
2016-11-25 17:56:23 +0100

    add model validation. Throw error on: negative dates, negative timespans

--- app/models/event.rb
+++ app/models/event.rb
@@ -6,13 +6,25 @@
 #  name             :string
 #  description      :string
 #  max_participants :integer
+#  date_ranges      :Collection
 #  active           :boolean
 #  created_at       :datetime         not null
 #  updated_at       :datetime         not null
 #
+
 class Event < ActiveRecord::Base
   has_many :application_letters
   has_many :date_ranges
 
   validates :max_participants, numericality: { only_integer: true, greater_than: 0 }
+
+
+
+
+  #TODO: This validation kills things.
+  # validate :has_date_ranges
+  #
+  #def has_date_ranges
+  #   errors.add(:base, 'Bitte mindestens eine Zeitspanne auswählen!') if date_ranges.blank?
+  #  end
 end
