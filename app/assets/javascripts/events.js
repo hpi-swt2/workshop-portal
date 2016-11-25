de19cf18cf829f38c7be272149dd3ba7ce0eaf07
Tom Beckmann <tomjonabc@gmail.com>
2016-11-26 00:24:12 +0100

    Fix adding date spans after turbolinks navigation

--- app/assets/javascripts/events.js
+++ app/assets/javascripts/events.js
@@ -1,8 +1,8 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 
-$(function() {
-  $('#event-add-date-picker').click(function() {
+$(document).on('turbolinks:load', function() {
+  $('#event-add-date-picker').bind('click', function() {
     var template = EVENT_DATE_PICKER_TEMPLATE;
 
     // insert our template to the ui and add a remove button
