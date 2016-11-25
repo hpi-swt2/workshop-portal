d96694618d9b5a195dd2b514c3b5518e522d3c74
Niklas Kiefer <niklas.kiefer@student.hpi.de>
2016-11-25 01:04:35 +0100

    #43 show participants list on badges-view, print badges by participant

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -44,34 +44,55 @@ class EventsController < ApplicationController
     @event.destroy
     redirect_to events_url, notice: 'Event was successfully destroyed.'
   end
-  
+
   # GET /events/1/badges
   def badges
     @event = Event.find(params[:event_id])
+    @participants = @event.participants
+
+    # TODO: undo mock
+    @participants.push(create_mock_participants)
+    @participants.push(create_mock_participants)
   end
 
   # POST /events/1/badges
   def print_badges
     @event = Event.find(params[:event_id])
+    @participants = @event.participants
+
+    # TODO: undo mock and get checked participants from view
+    @participants.push(create_mock_participants)
+    @participants.push(create_mock_participants)
+    @participants.push(create_mock_participants)
+    @participants.push(create_mock_participants)
 
     # pdf document initialization
     pdf = Prawn::Document.new(:page_size => 'A4')
     pdf.stroke_color "000000"
 
-    # create badge edges as rectangles left-upper-bound[x,y], width, height
+    # creates badge edges as rectangles left-upper-bound[x,y], width, height
+    # TODO: create dynamically from checked participants
+    @participants.each_with_index do |participant, index|
+      if index % 2 == 0 # left column badges
+        create_badge(pdf, 0, 750 - index / 2 * 150)
+        index = index - 1
+      else
+        create_badge(pdf, 260, 750 - index / 2 * 150)
+      end
 
-    # TODO: create dynamically from participants
-    create_badge(pdf, 0, 750)
-    create_badge(pdf, 0, 600)
-    create_badge(pdf, 0, 450)
-    create_badge(pdf, 0, 300)
-    create_badge(pdf, 0, 150)
+    end
 
-    create_badge(pdf, 260, 750)
-    create_badge(pdf, 260, 600)
-    create_badge(pdf, 260, 450)
-    create_badge(pdf, 260, 300)
-    create_badge(pdf, 260, 150)
+    #create_badge(pdf, 0, 750)
+    #create_badge(pdf, 0, 600)
+    #create_badge(pdf, 0, 450)
+    #create_badge(pdf, 0, 300)
+    #create_badge(pdf, 0, 150)
+
+    #create_badge(pdf, 260, 750)
+    #create_badge(pdf, 260, 600)
+    #create_badge(pdf, 260, 450)
+    #create_badge(pdf, 260, 300)
+    #create_badge(pdf, 260, 150)
 
     send_data pdf.render, :filename => "x.pdf", :type => "application/pdf", disposition: "inline"
   end
@@ -99,5 +120,9 @@ class EventsController < ApplicationController
       pdf.stroke_rectangle [x, y], width, height
       # TODO: the position is very hacky, make it better
       pdf.draw_text "Max Mustermann", :at => [x + width / 2 - 50 , y - 20]
+    end
+
+    def create_mock_participants
+      participant = User.new(name: "Max Mustermann")
     end
 end
--- spec/views/events/badges.html.erb_spec.rb
+++ spec/views/events/badges.html.erb_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe "events/badges", type: :view do
   before(:each) do
     @event = assign(:event, FactoryGirl.create(:event))
+    @participants = assign(:participants, @event.participants)
   end
 
   it "renders the print badges event page" do
@@ -12,6 +13,11 @@ RSpec.describe "events/badges", type: :view do
 
   it "renders the generate pdf button" do
     render
-    assert_select "a", :text => 'generate PDF'
+    assert_select "a", :text => "generate PDF"
+  end
+
+  it "renders the list of the participants" do
+    render
+    assert_select "h1", :text => "Teilnehmer"
   end
 end
