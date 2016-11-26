c8929694bdb97b116885508cee11cca34643f449
TimBasel <tim.basel@student.hpi.de>
2016-11-26 16:49:24 +0100

    Failing Test for incorrect age method, refactored feature tests for #23

--- spec/features/events_spec.rb
+++ spec/features/events_spec.rb
@@ -2,33 +2,26 @@ require "rails_helper"
 
 RSpec.feature "Event Applicant Overview", :type => :feature do
   scenario "logged in as Pupil can not see overview" do
-    @event = FactoryGirl.create(:event)
-    @profile = FactoryGirl.create(:profile)
-    @profile.user.role = :pupil
-    login_as(@profile.user, :scope => :user)
-    visit event_path(@event)
-
+    login(:pupil)
     expect(page).to_not have_table("applicants")
   end
 
   scenario "logged in as Coach can see overview" do
-    @event = FactoryGirl.create(:event)
-    @profile = FactoryGirl.create(:profile)
-    @profile.user.role = :tutor
-    login_as(@profile.user, :scope => :user)
-    visit event_path(@event)
-
+    login(:tutor)
     expect(page).to have_table("applicants")
   end
 
   scenario "logged in as Organizer can see overview" do
-    @event = FactoryGirl.create(:event)
-    @profile = FactoryGirl.create(:profile)
-    @profile.user.role = :organizer
-    login_as(@profile.user, :scope => :user)
-    visit event_path(@event)
-
+    login(:organizer)
     expect(page).to have_table("applicants")
   end
 
+
+  def login(role)
+    @event = FactoryGirl.create(:event)
+    @profile = FactoryGirl.create(:profile)
+    @profile.user.role = role
+    login_as(@profile.user, :scope => :user)
+    visit event_path(@event)
+  end
 end
