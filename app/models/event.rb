82d5cebea63521a20992e18e9ccfd680afe26f63
Frederike Ramin <frederike.ramin@student.hpi.de>
2016-11-27 13:26:34 +0100

    Add documentation for #29

--- app/models/event.rb
+++ app/models/event.rb
@@ -15,10 +15,18 @@ class Event < ActiveRecord::Base
 
   validates :max_participants, numericality: { only_integer: true, greater_than: 0 }
 
+  # Returns the number of free places of the event, this value may be negative
+  #
+  # @param none
+  # @return [Int] for number of free places available
   def compute_free_places
     max_participants - compute_occupied_places
   end
 
+  # Returns the number of already occupied places of the event
+  #
+  # @param none
+  # @return [Int] for number of occupied places
   def compute_occupied_places
     application_letters.where(status: true).count
   end
