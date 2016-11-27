4a64dd6b8f21b72f0fb0a38a3e8f50d6b8004490
Fabian Sommer <fabiansbg@web.de>
2016-11-27 12:14:09 +0100

    Colorized text on participants overview. Added tests for letter of agreement detection

--- spec/views/events/participants.html.erb_spec.rb
+++ spec/views/events/participants.html.erb_spec.rb
@@ -11,4 +11,41 @@ RSpec.describe "events/participants", type: :view do
     expect(rendered).to have_text("Einverständniserklärung")
     expect(rendered).to have_text(@event.participants[0].name)
   end
+  
+  it "detects missing agreement letters" do
+	@user = FactoryGirl.create(:user)
+	@profile = FactoryGirl.create(:profile, user: @user, birth_date: 15.years.ago)
+	@event = FactoryGirl.create(:event)
+	@application = FactoryGirl.create(:accepted_application_letter, user: @user, event: @event)
+	assign(:event, @event)
+	assign(:participants, @event.participants)
+	render
+	expect(rendered).to have_text("nicht vorhanden")
+  end
+  
+  it "detects available agreement letters" do
+	@user = FactoryGirl.create(:user)
+	@profile = FactoryGirl.create(:profile, user: @user, birth_date: 15.years.ago)
+	@event = FactoryGirl.create(:event)
+	@application = FactoryGirl.create(:accepted_application_letter, user: @user, event: @event)
+	@agreement = FactoryGirl.create(:agreement_letter, user: @user, event: @event)
+	assign(:event, @event)
+	assign(:participants, @event.participants)
+	render
+	expect(rendered).to have_text("vorhanden")
+	expect(rendered).not_to have_text("nicht vorhanden")
+  end
+  
+  it "detects when agreement letters are unnecessary" do
+	@user = FactoryGirl.create(:user)
+	@profile = FactoryGirl.create(:profile, user: @user, birth_date: 19.years.ago)
+	@event = FactoryGirl.create(:event)
+	@application = FactoryGirl.create(:accepted_application_letter, user: @user, event: @event)
+	@agreement = FactoryGirl.create(:agreement_letter, user: @user, event: @event)
+	assign(:event, @event)
+	assign(:participants, @event.participants)
+	render
+	expect(rendered).to have_text("unnötig")
+  end
+  
 end
