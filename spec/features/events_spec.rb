484aed2760b55b78ca2779f9ea01c248d947285b
Lando-L <lando.loeper@gmx.net>
2016-11-27 13:42:54 +0100

    Featured Issue #22 Added failing test

--- spec/features/events_spec.rb
+++ spec/features/events_spec.rb
@@ -16,6 +16,26 @@ RSpec.feature "Event Applicant Overview", :type => :feature do
     expect(page).to have_table("applicants")
   end
 
+  scenario "Emails can not be send if there is any unclassified application left" do
+    login(:organizer)
+    expect(page).to have_button("Zusagen verschicken", disabled: true)
+    expect(page).to have_button("Absagen verschicken", disabled: true)
+  end
+
+  scenario "Emails can not be send if there is a negative number of free places left" do
+    login(:organizer)
+    page.choose("Accept")
+    expect(page).to have_button("Zusagen verschicken", disabled: true)
+    expect(page).to have_button("Absagen verschicken", disabled: true)
+  end
+
+  scenario "Clicking on sending emails button opens a modal" do
+    login(:organizer)
+    page.choose("Accept")
+    expect(page).to have_button("Zusagen verschicken", disabled: false)
+    expect(page).to have_button("Absagen verschicken", disabled: false)
+  end
+
 
   def login(role)
     @event = FactoryGirl.create(:event)
