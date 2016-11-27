175397e781e86c4d4f0f0a3c8cae6b89e3b8dc38
Alexander Riese <alexander.riese@student.hpi.de>
2016-11-27 23:31:57 +0100

    Failing test for #45: change application status with radio buttons

--- spec/features/events_spec.rb
+++ spec/features/events_spec.rb
@@ -31,4 +31,21 @@ RSpec.feature "Event Applicant Overview", :type => :feature do
     expect(page).to have_table("applicants")
   end
 
+  scenario "logged in as Coach can change application status with radio buttons" do
+    @event = FactoryGirl.create(:event)
+    @profile = FactoryGirl.create(:profile)
+    @profile.user.role = :tutor
+    login_as(@profile.user, :scope => :user)
+    visit event_path(@event)
+
+    @pupil = FactoryGirl.create(:profile)
+    @application_letter = FactoryGirl.create(:application_letter, event: @event, user: @pupil.user)
+    visit event_path(@event)
+    ApplicationLetter.statuses.keys.each do |new_status|
+      choose(I18n.t "application_status.#{new_status}")
+      ApplicationLetter.where(id: @application_letter.id, status: new_status)
+    end
+
+  end
+
 end
