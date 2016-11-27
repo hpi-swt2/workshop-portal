9f2a78ca3f3adfa228d1f2ef0eb78deeb8b26a96
Frederike Ramin <frederike.ramin@student.hpi.de>
2016-11-27 13:13:25 +0100

    Add documentation for #24

--- app/models/application_note.rb
+++ app/models/application_note.rb
@@ -1,3 +1,13 @@
+# == Schema Information
+#
+# Table name: application_notes
+#
+#  id                     :integer          not null, primary key
+#  application_letter_id  :integer          not null, foreign key
+#  note                   :text             not null
+#  created_at             :datetime         not null
+#  updated_at             :datetime         not null
+#
 class ApplicationNote < ActiveRecord::Base
   belongs_to :application_letter
 
