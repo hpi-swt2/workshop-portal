47859c1f315de4a10aa3ea7c30aa3873601cd991
Carl Gödecken <carl.goedecken@student.hpi.de>
2016-11-28 16:34:13 +0100

    Permit additional attributes to be saved

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -53,6 +53,6 @@ class EventsController < ApplicationController
 
     # Only allow a trusted parameter "white list" through.
     def event_params
-      params.require(:event).permit(:name, :description, :max_participants, :active)
+      params.require(:event).permit(:name, :description, :max_participants, :active, :organizer, :knowledge_level)
     end
 end
