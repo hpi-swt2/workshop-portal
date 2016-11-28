0e82d5c2eead1ff9edc809e97da44f6f9f6bb383
Alexander Löser <alexander.loeser@student.hpi.uni-potsdam.de>
2016-11-28 22:59:10 +0100

    removed migration change in create_applications migration #29

--- db/migrate/20161103101916_create_applications.rb
+++ db/migrate/20161103101916_create_applications.rb
@@ -3,7 +3,7 @@ class CreateApplications < ActiveRecord::Migration
     create_table :applications do |t|
       t.string :motivation
       t.references :user, index: true, foreign_key: true
-      t.references :event, index: true, foreign_key: true
+      t.references :workshop, index: true, foreign_key: true
 
       t.timestamps null: false
     end
