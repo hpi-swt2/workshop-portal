73190eee642ab2b8358b7d1fdfe0cb78470cc44e
Carl Gödecken <carl.goedecken@student.hpi.de>
2016-11-29 23:57:58 +0100

    Make migration conditional
    This fixes migration issues for users coming from current dev

--- db/migrate/20161129111916_add_knowledge_level_to_workshops.rb
+++ db/migrate/20161129111916_add_knowledge_level_to_workshops.rb
@@ -1,5 +1,10 @@
 class AddKnowledgeLevelToWorkshops < ActiveRecord::Migration
-  def change
-    add_column :events, :knowledge_level, :string
+  def self.up
+    add_column :events, :knowledge_level, :string unless column_exists?(:events, :knowledge_level)
+  end
+
+  def self.down
+    Profile.reset_column_information
+    remove_column :events, :knowledge_level, :string if column_exists?(:events, :knowledge_level)
   end
 end
