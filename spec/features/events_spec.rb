ae3de3f3a58deafd7d07013fa2d265c43280a925
Christian Flach <cmfcmf.flach@gmail.com>
2016-11-30 19:31:28 +0100

    Further renaming...

--- spec/features/events_spec.rb
+++ spec/features/events_spec.rb
@@ -50,7 +50,7 @@ RSpec.feature "Event Applicant Overview", :type => :feature do
   end
 
   scenario "logged in as Coach I can see application status" do
-    login(:tutor)
+    login(:coach)
 
     @pupil = FactoryGirl.create(:profile)
     @application_letter = FactoryGirl.create(:application_letter, event: @event, user: @pupil.user)
