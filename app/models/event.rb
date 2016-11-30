db3fbca801be82262c3713205db9a1a24fcb67a9
Lando-L <lando.loeper@gmx.net>
2016-11-30 15:50:20 +0100

    Featured Issue #22 added comment to new event model methods

--- app/models/event.rb
+++ app/models/event.rb
@@ -15,15 +15,27 @@ class Event < ActiveRecord::Base
 
   validates :max_participants, numericality: { only_integer: true, greater_than: 0 }
 
+  # Returns whether all application_letters are classified or not
+  #
+  # @param none
+  # @return [Boolean] if all application_letter are classified
   def applicationsClassified?
     application_letters.all? { |application_letter| !application_letter.status.nil? }
   end
 
+  # Returns a string of all email addresses of rejected applications
+  #
+  # @param none
+  # @return [String] Concatenation of all email addresses of rejected applications, seperated by ','
   def compute_rejected_applications_emails
     rejected_applications = application_letters.select { |application_letter| !application_letter.status }
     rejected_applications.map{ |applications_letter| applications_letter.user.email }.join(',')
   end
 
+  # Returns a string of all email addresses of accepted applications
+  #
+  # @param none
+  # @return [String] Concatenation of all email addresses of accepted applications, seperated by ','
   def compute_accepted_applications_emails
     accepted_applications = application_letters.select { |application_letter| application_letter.status }
     accepted_applications.map{ |application_letter| application_letter.user.email }.join(',')
