bed5d814af3fa0733ead8b9357daf9b6f3d553a6
Lukas Behrendt <lukas.behrendt@student.hpi.de>
2016-12-01 17:29:23 +0100

    Add test for print button Phew that was hard

--- spec/views/events/show.html.erb_spec.rb
+++ spec/views/events/show.html.erb_spec.rb
@@ -51,4 +51,9 @@ RSpec.describe "events/show", type: :view do
     render
     expect(rendered).to have_link(t(:details, scope: 'events.applicants_overview'))
   end
+
+  it "displays print applications button" do
+    render
+    expect(rendered).to have_link(t(:print_all, scope: 'events.applicants_overview'))
+  end
 end
