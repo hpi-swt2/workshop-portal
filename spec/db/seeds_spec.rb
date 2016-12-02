7161916d417046939c2bd70267453f6b0e418c3f
Christian Flach <cmfcmf.flach@gmail.com>
2016-12-02 15:59:13 +0100

    Make seed test a bit clearer.

--- spec/db/seeds_spec.rb
+++ spec/db/seeds_spec.rb
@@ -2,6 +2,8 @@ require 'rails_helper'
 
 describe 'Seeds' do
   it 'should seed the database without errors' do
-    Rails.application.load_seed
+    expect{
+      Rails.application.load_seed
+    }.to_not raise_error
   end
 end
