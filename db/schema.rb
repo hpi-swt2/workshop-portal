cfe62ad5fdfa8e48c185ffa948766a8bc96fe322
Nils Strelow <nils.strelow@student.hpi.de>
2016-12-05 00:45:40 +0100

    add db schema so travis is happy

--- db/schema.rb
+++ db/schema.rb
@@ -11,8 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20161123124500) do
+ActiveRecord::Schema.define(version: 20161204205355) do
 
   create_table "application_letters", force: :cascade do |t|
     t.string   "motivation"
@@ -26,17 +25,6 @@ ActiveRecord::Schema.define(version: 20161123124500) do
   add_index "application_letters", ["event_id"], name: "index_application_letters_on_event_id"
   add_index "application_letters", ["user_id"], name: "index_application_letters_on_user_id"
 
-  create_table "events", force: :cascade do |t|
-    t.string   "name"
-    t.string   "description"
-    t.integer  "max_participants"
-    t.boolean  "active"
-    t.datetime "created_at",       null: false
-    t.datetime "updated_at",       null: false
-    t.string   "organizer"
-    t.string   "knowledge_level"
-  end
-
   create_table "application_notes", force: :cascade do |t|
     t.text     "note"
     t.integer  "application_letter_id"
@@ -45,6 +33,18 @@ ActiveRecord::Schema.define(version: 20161123124500) do
   end
 
   add_index "application_notes", ["application_letter_id"], name: "index_application_notes_on_application_letter_id"
+
+  create_table "events", force: :cascade do |t|
+    t.string   "name"
+    t.string   "description"
+    t.integer  "max_participants"
+    t.boolean  "active"
+    t.datetime "created_at",           null: false
+    t.datetime "updated_at",           null: false
+    t.string   "organizer"
+    t.string   "knowledge_level"
+    t.date     "application_deadline"
+  end
 
   create_table "profiles", force: :cascade do |t|
     t.string   "cv"
