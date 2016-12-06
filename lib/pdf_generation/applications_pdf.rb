d093ef84700a5d423cc23164736ecd857c831613
Lennart L <lennart.lehmann@student.hpi.de>
2016-12-06 16:19:59 +0100

    print basic application detail page #30

--- lib/pdf_generation/applications_pdf.rb
+++ lib/pdf_generation/applications_pdf.rb
@@ -22,5 +22,13 @@ class ApplicationsPDF
     end
 
     def create_application_page(application_letter)
+      start_new_page
+      text "Application from #{application_letter.user.profile.name}", size: 20
+      text "for #{application_letter.event.name}", size: 14
+      stroke_horizontal_rule
+
+      pad_top(20) { text "Status: #{application_letter.status}" }
+      pad(10) { text "Motivation letter:" }
+      text application_letter.motivation
     end
 end
