7a66d6741c5e2c6b8b09a969c9d2f738d3c61d58
Christian Flach <cmfcmf.flach@gmail.com>
2016-12-06 10:12:44 +0100

    Simplify tests.

--- spec/features/application_letters_spec.rb
+++ spec/features/application_letters_spec.rb
@@ -9,34 +9,21 @@ RSpec.feature "Application Letter Overview", :type => :feature do
     end
   end
 
-  scenario "logged in as Coach I can see notes" do
-    login(:coach)
-    expect(page).to have_css("textarea#application_note_note")
-    @application_letter.application_notes.each do | note |
-      expect(page).to have_text(note.note)
+  %i[coach organizer].each do |role|
+    scenario "logged in as #{role} I can see notes" do
+      login(role)
+      expect(page).to have_css("textarea#application_note_note")
+      @application_letter.application_notes.each do | note |
+        expect(page).to have_text(note.note)
+      end
     end
-  end
 
-  scenario "logged in as Organizer I can see notes" do
-    login(:organizer)
-    expect(page).to have_css("textarea#application_note_note")
-    @application_letter.application_notes.each do | note |
-      expect(page).to have_text(note.note)
+    scenario "logged in as #{role} I can create new notes" do
+      login(role)
+      fill_in("application_note_note", with: "Hate him! Hate him!")
+      click_button I18n.t "helpers.titles.new", model: ApplicationNote.model_name.human
+      expect(page).to have_text("Hate him! Hate him!")
     end
-  end
-
-  scenario "logged in as Organizer I can create new notes" do
-    login(:organizer)
-    fill_in("application_note_note", with: "Hate him! Hate him!")
-    click_button I18n.t "helpers.titles.new", model: ApplicationNote.model_name.human
-    expect(page).to have_text("Hate him! Hate him!")
-  end
-
-  scenario "logged in as Coach I can create new notes" do
-    login(:coach)
-    fill_in("application_note_note", with: "Hate him! Hate him!")
-    click_button I18n.t "helpers.titles.new", model: ApplicationNote.model_name.human
-    expect(page).to have_text("Hate him! Hate him!")
   end
 
   it "should highlight wrong or missing insertions from user" do
@@ -88,7 +75,6 @@ RSpec.feature "Application Letter Overview", :type => :feature do
   end
 
   def login(role)
-
     @event = FactoryGirl.create(:event)
     @profile = FactoryGirl.create(:profile)
     @profile.user.role = role
