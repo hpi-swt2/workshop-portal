ea1e650b619f5741c5a9e3d22faeb527e0b1e5a9
Fabian-Sommer <fabiansbg@web.de>
2016-12-07 12:28:12 +0100

    26 4.2 check letter of agreement (#74)
    * Add feature tests for specifying date ranges on events
    
    * create dateRange model test, dateRange factory. Add eventRanges to  workshop-factory
    
    * Add basic ui for adding and removing date pickers
    
    * Failing tests for #26.
    
    * Add upload form to every profile page
    
    * Created participant view for a workshop.
    
    * fixed failing test
    
    * Fix tests
    
    * Add traits to eventfactory, add tests for event.date_ranges, refactor workshop->event renaming things
    
    * add failing traits for date_ranges factory. Add date_range validation tests, add event test for event without ranges
    
    * Adapt tests to work on page output
    
    * Add date_range model. Add respective migration. Add relation: event->HMBT<-date_range
    
    * fix syntax errors in date_range model and event model tests
    
    * actually fix last event model test
    
    * Show errors if uploaded file is too big or not a PDF
    
    * fix magic numbers -> now forever tomorrow
    
    * fix magic numbers -> now also in factories forever tomorrow
    
    * add model validation. Throw error on: negative dates, negative timespans
    
    * Make saving events work
    
    * add validation for start_date
    
    * Created test for controller in order to have 100% test coverage
    
    * Event creation throws errors for: event dates in the past, end_date before start_date, unreasonable timespan.
    
    * Failing tests for #start_date/#end_date on Event
    
    * Add #end_date and #start_date to Event model
    
    * Change some english labels to german
    
    * Show warning for events with unreasonably long timespans
    
    * Failing tests for edit event's date ranges
    
    * List existing date ranges on edit page
    
    * Make editing date ranges on events work
    
    * Fix adding date spans after turbolinks navigation
    
    * Save uploaded PDFs on server
    
    * Minor cleanup, indent fixes
    
    * 100% code coverage is stupid sometimes
    
    * Save PDFs to the right event
    
    * Re-enable validation to check for events having at least a single date range
    
    * Use proper label function in show
    
    * Add tests for overwriting file
    
    * Added additional test for participants. Added failing test for agreement letter.
    
    * Created method in user to check if agreement letter for a event is available
    
    * Added additional function to user to return agreement letter for given event.
    
    * Added function to test whether participant is older than 18 at event
    
    * fixed failing test after merge
    
    * added row to participants view that shows if application letter is supplied
    
    * fixed some tests
    
    * fixed all remaining tests
    
    * Colorized text on participants overview. Added tests for letter of agreement detection
    
    * Participants page is now ordered by letter of agreement status.
    
    * merging
    
    * more merging
    
    * Added i18n for participants overview.
    Refactoring.
    
    * Implementing requested changes
    
    * Added cancans and a button to get to the page
    
    * Added documentation
    
    * Fixed failing tests.
    
    * Improved documentation
    
    * Improved documentation

--- spec/views/events/participants.html.erb_spec.rb
+++ spec/views/events/participants.html.erb_spec.rb
@@ -4,11 +4,49 @@ RSpec.describe "events/participants", type: :view do
   before(:each) do
     @event = assign(:event, FactoryGirl.create(:event_with_accepted_applications))
     @participants = assign(:participants, @event.participants)
+    sign_in(FactoryGirl.create(:user, role: :admin))
   end
 
   it "renders attributes" do
     render
-    expect(rendered).to have_text("Einverständniserklärung")
+    expect(rendered).to have_text(t(:participants, scope:'events.participants'))
     expect(rendered).to have_text(@event.participants[0].name)
   end
+  
+  it "detects missing agreement letters" do
+    @user = FactoryGirl.create(:user)
+    @profile = FactoryGirl.create(:profile, user: @user, birth_date: 15.years.ago)
+    @event = FactoryGirl.create(:event)
+    @application = FactoryGirl.create(:application_letter_accepted, user: @user, event: @event)
+    assign(:event, @event)
+    assign(:participants, @event.participants)
+    render
+    expect(rendered).to have_text(t(:unavailable, scope:'events.participants'))
+  end
+  
+  it "detects available agreement letters" do
+    @user = FactoryGirl.create(:user)
+    @profile = FactoryGirl.create(:profile, user: @user, birth_date: 15.years.ago)
+    @event = FactoryGirl.create(:event)
+    @application = FactoryGirl.create(:application_letter_accepted, user: @user, event: @event)
+    @agreement = FactoryGirl.create(:agreement_letter, user: @user, event: @event)
+    assign(:event, @event)
+    assign(:participants, @event.participants)
+    render
+    expect(rendered).to have_text(t(:available, scope:'events.participants'))
+    expect(rendered).not_to have_text(t(:unavailable, scope:'events.participants'))
+  end
+  
+  it "detects when agreement letters are unnecessary" do
+    @user = FactoryGirl.create(:user)
+    @profile = FactoryGirl.create(:profile, user: @user, birth_date: 19.years.ago)
+    @event = FactoryGirl.create(:event)
+    @application = FactoryGirl.create(:application_letter_accepted, user: @user, event: @event)
+    @agreement = FactoryGirl.create(:agreement_letter, user: @user, event: @event)
+    assign(:event, @event)
+    assign(:participants, @event.participants)
+    render
+    expect(rendered).to have_text(t(:unnecessary, scope:'events.participants'))
+  end
+  
 end
