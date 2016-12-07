8dfbe6f73a49f2806557cfd8ce89045a9253b56b
Frederike Ramin <frederike.ramin@student.hpi.de>
2016-12-07 14:18:25 +0100

    Add reply_to field #55

--- app/mailers/portal_mailer.rb
+++ app/mailers/portal_mailer.rb
@@ -1,10 +1,10 @@
 class PortalMailer < ApplicationMailer
-  def generic_email(hideRecipients, recipients, subject, content)
+  def generic_email(hideRecipients, recipients, reply_to, subject, content)
     @content = content
     if (hideRecipients)
-      mail(bcc: recipients, subject: subject)
+      mail(bcc: recipients, reply_to: reply_to, subject: subject)
     else
-      mail(to: recipients, subject: subject)
+      mail(to: recipients, reply_to: reply_to, subject: subject)
     end
   end
 end
