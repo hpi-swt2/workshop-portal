c8b00b4982b9fcc9f5b226989fe20aab4b2791f0
Christian Flach <cmfcmf@users.noreply.github.com>
2016-12-07 16:33:48 +0100

    Skip JS tests if driver is not installed. (#156)

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -26,6 +26,9 @@ RSpec.configure do |config|
 
   Capybara.javascript_driver = :poltergeist
 
+  # Skip PhantomJS tests if PhantomJS is not installed.
+  config.filter_run_excluding :js => (not Cliver::detect('phantomjs'))
+
   # Use color not only in STDOUT but also in pagers and files
   # config.tty = true
 
