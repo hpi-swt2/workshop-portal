c904082bdd22dc35c0cbcb7787f32d2f6f02aeca
Frederike Ramin <frederike.ramin@student.hpi.de>
2016-12-09 00:00:39 +0100

    Changed documentation #55

--- app/mailers/portal_mailer.rb
+++ app/mailers/portal_mailer.rb
@@ -1,10 +1,14 @@
 class PortalMailer < ApplicationMailer
 
-  # @param hideRecipients [Boolean], recipients [Array<String>], reply_to [Array<String>], subject [String], content [String]
+  # @param hide_recipients [Boolean] - identify whether recipients should be hidden from each other (true) or not (false)
+  # @param recipients [Array<String>] - email addresses of recipients
+  # @param reply_to [Array<String>] - email addresses of recipient of the answer
+  # @param subject [String] - subject of the mail
+  # @param content [String] - content of the mail
   # @return [ActionMailer::MessageDelivery] a mail object with the given parameters.
-  def generic_email(hideRecipients, recipients, reply_to, subject, content)
+  def generic_email(hide_recipients, recipients, reply_to, subject, content)
     @content = content
-    if (hideRecipients)
+    if (hide_recipients)
       mail(bcc: recipients, reply_to: reply_to, subject: subject)
     else
       mail(to: recipients, reply_to: reply_to, subject: subject)
