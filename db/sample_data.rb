d2a3c8d3c92376317441c2d86c764e65c092cda0
Nils Thamm <nils.thamm@student.hpi.de>
2016-12-09 15:35:20 +0100

    Added profile sample data

--- db/sample_data.rb
+++ db/sample_data.rb
@@ -11,6 +11,9 @@ def add_sample_data
   users[:applicant] = user_applicant
 
   profiles = Hash.new
+  profiles[:pupil] = profile_pupil(users[:pupil])
+  profiles[:teacher] = profile_teacher(users[:teacher])
+  profiles[:applicant] = profile_applicant(users[:applicant])
 
 
   [events, users, profiles].each do |models|
@@ -144,5 +147,56 @@ private
       email: "bewerber@example.com",
       password: user_password,
       role: :pupil
+    )
+  end
+
+  def profile_pupil(user)
+    Profile.new(
+      user: user,
+      first_name: "Karl",
+      last_name: "Doe",
+      gender: "männlich",
+      birth_date: Date.parse('2000.11.29'),
+      school: "Schule am Griebnitzsee",
+      street_name: "Rudolf-Breitscheid-Str. 52",
+      zip_code: "14482",
+      city: "Potsdam",
+      state: "Brandenburg",
+      country: "Deutschland",
+      graduates_school_in: "2019"
+    )
+  end
+
+  def profile_teacher(user)
+    Profile.new(
+      user: user,
+      first_name: "Ernst",
+      last_name: "Teacher",
+      gender: "männlich",
+      birth_date: Date.parse('1970.1.1'),
+      school: "Schule am Griebnitzsee",
+      street_name: "Domstraße 14",
+      zip_code: "14482",
+      city: "Potsdam",
+      state: "Brandenburg",
+      country: "Deutschland",
+      graduates_school_in: "Bereits Abitur"
+    )
+  end
+
+  def profile_applicant(user)
+    Profile.new(
+      user: user,
+      first_name: "Erika",
+      last_name: "Mustermann",
+      gender: "weiblich",
+      birth_date: Date.parse('1999.08.14'),
+      school: "Schule am Griebnitzsee",
+      street_name: "Rudolf-Breitscheid-Str. 52",
+      zip_code: "14482",
+      city: "Potsdam",
+      state: "Brandenburg",
+      country: "Deutschland",
+      graduates_school_in: "2017"
     )
   end
