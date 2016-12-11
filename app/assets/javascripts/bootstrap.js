4a08927cc64a1bc7396eb667f697fa8fe53d8609
Tom Beckmann <tomjonabc@gmail.com>
2016-12-11 15:07:11 +0100

    Remove animation disabler

--- app/assets/javascripts/bootstrap.js
+++ app/assets/javascripts/bootstrap.js
@@ -1,4 +1,10 @@
-jQuery(function() {
+function enableBootstrapComponents() {
   $("a[rel~=popover], .has-popover").popover();
   $("a[rel~=tooltip], .has-tooltip").tooltip();
-});
+}
+
+// make sure we activate on first load (in particular tests don't work well
+// with just turbolinks:load) and also on any subsequent page change via
+// turbolinks
+$(document).on('turbolinks:load', enableBootstrapComponents);
+jQuery(enableBootstrapComponents);
