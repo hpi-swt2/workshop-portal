e0b49a7b9da4af632fadde56b8787f99e979a35d
Kim Borchart <Kim-Pascal.Borchart@student.hpi.de>
2016-12-13 11:42:53 +0100

    single quotes in event_spec

--- spec/features/event_spec.rb
+++ spec/features/event_spec.rb
@@ -17,7 +17,7 @@ describe "Event", type: :feature do
       select_date_within_selector(Date.yesterday.prev_day, '.event-date-picker-start')
       select_date_within_selector(Date.yesterday, '.event-date-picker-end')
       click_button I18n.t('.events.form.publish')
-      expect(page).to have_text("Anfangs-Datum darf nicht in der Vergangenheit liegen")
+      expect(page).to have_text('Anfangs-Datum darf nicht in der Vergangenheit liegen')
     end
 
     it "should warn about unreasonably long time spans" do
@@ -26,7 +26,7 @@ describe "Event", type: :feature do
       select_date_within_selector(Date.today, '.event-date-picker-start')
       select_date_within_selector(Date.today.next_year(3), '.event-date-picker-end')
       click_button I18n.t('.events.form.publish')
-      expect(page).to have_text("End-Datum liegt ungewöhnlich weit vom Start-Datum entfernt.")
+      expect(page).to have_text('End-Datum liegt ungewöhnlich weit vom Start-Datum entfernt.')
     end
 
     it "should not allow an end date before a start date" do
@@ -35,7 +35,7 @@ describe "Event", type: :feature do
       select_date_within_selector(Date.today.prev_day(2), '.event-date-picker-end')
       click_button I18n.t('.events.form.publish')
 
-      expect(page).to have_text("End-Datum kann nicht vor Start-Datum liegen")
+      expect(page).to have_text('End-Datum kann nicht vor Start-Datum liegen')
     end
 
     it "should allow entering multiple time spans", js: true do
