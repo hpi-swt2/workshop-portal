03c9d844792131a5e4c522fb3094d6db1eb3de12
Frederike Ramin <frederike.ramin@student.hpi.de>
2016-12-14 13:36:06 +0100

    Failing tests for #55 avoid bcc

--- spec/services/mailer_spec.rb
+++ spec/services/mailer_spec.rb
@@ -2,6 +2,7 @@ require "rails_helper"
 
 RSpec.describe Mailer, type: :service do
   let(:recipients) { ['test@example.de', 'test2@example.de'] }
+  let(:recipients_string) { 'test@example.de, test2@example.de' }
   let(:reply_to) { ['test3@example.de'] }
   let(:subject) {'Subject'}
   let(:content) {'Awesome content'}
@@ -18,9 +19,20 @@ RSpec.describe Mailer, type: :service do
       expect{mailer.send_generic_email(true, recipients, reply_to, subject, content)}.to change{ActionMailer::Base.deliveries.count}.by(recipients.count)
     end
 
-    it 'hides recipients from each other' do
+    it 'sends right amount of emails' do
+      expect{mailer.send_generic_email(true, recipients_string, reply_to, subject, content)}.to change{ActionMailer::Base.deliveries.count}.by(recipients.count)
+    end
+
+    it 'hides recipients from each other with array' do
       mailer.send_generic_email(true, recipients, reply_to, subject, content)
       ActionMailer::Base.deliveries.last(recipients.count).each do |delivery|
+        expect(delivery.to.count).to eq(1)
+      end
+    end
+
+    it 'hides recipients from each other with string' do
+      mailer.send_generic_email(true, recipients_string, reply_to, subject, content)
+      ActionMailer::Base.deliveries.last((recipients_string.count ',') + 1).each do |delivery|
         expect(delivery.to.count).to eq(1)
       end
     end
