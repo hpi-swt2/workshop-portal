054c5e38fd77eade1d10ef154293af4c83ad7500
Alexander Löser <alexander.loeser@student.hpi.uni-potsdam.de>
2016-12-14 19:49:00 +0100

    more documentation for #55 avoid bcc

--- app/services/mailer.rb
+++ app/services/mailer.rb
@@ -1,4 +1,11 @@
 class Mailer
+
+  # @param hide_recipients - a boolean. `true` will send an email to the recipients one by one, `false` will send an email to all at once
+  # @param recipients [Array<String>] - email addresses of recipients - can be a string of comma separated email adresses too
+  # @param reply_to [Array<String>] - email addresses of recipient of the answer - can be a string of comma separated email adresses too
+  # @param subject [String] - subject of the mail
+  # @param content [String] - content of the mail
+  # @return [ActionMailer::MessageDelivery] a mail object with the given parameters.
   def self.send_generic_email(hide_recipients, recipients, reply_to, subject, content)
     if hide_recipients
       if recipients.is_a? String
