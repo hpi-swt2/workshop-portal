a66a5148393836110895ddb3b23ab36d0214a925
Frederike Ramin <frederike.ramin@student.hpi.de>
2016-12-14 14:01:31 +0100

    Implement avoid bcc with strings for #55

--- app/services/mailer.rb
+++ app/services/mailer.rb
@@ -1,6 +1,9 @@
 class Mailer
   def send_generic_email(hide_recipients, recipients, reply_to, subject, content)
     if hide_recipients
+      if recipients.is_a? String
+        recipients = recipients.lines(',')
+      end
       recipients.each do |recipient|
         PortalMailer.generic_email(recipient, reply_to, subject, content).deliver_now
       end
