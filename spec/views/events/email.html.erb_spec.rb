afa523e4436cd435766c93d8bc6a2a9e324366dc
Tim Basel <tim.basel@student.hpi.de>
2016-12-15 00:55:08 +0100

    Removed unused test file

--- spec/views/events/email.html.erb_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe "events/send_email", type: :view do
-  before(:each) do
-    @event = assign(:event, FactoryGirl.create(:event_with_accepted_applications))
-    @adresses = assign(:adresses, @event.email_adresses_of_accepted_applicants)
-    sign_in(FactoryGirl.create(:user, role: :admin))
-  end
-end
