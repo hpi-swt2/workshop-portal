fe8a8ca4651b19afe04b92bfd958008c048f4909
Alexander Löser <alexander.loeser@student.hpi.de>
2016-12-14 20:57:33 +0100

    55 3.14 send emails backend avoid bcc (#196)
    * Failing tests for #55 avoid bcc
    
    * Implement bcc avoiding for #55
    
    * Failing tests for #55 avoid bcc
    
    * Implement avoid bcc with strings for #55
    
    * Make send_email a class method #55
    
    * remove hide_recipients from the documentation #55
    
    * Improved tests for avoiding bcc #55
    
    * more documentation for #55 avoid bcc

--- spec/mailers/previews/portal_mailer_preview.rb
+++ spec/mailers/previews/portal_mailer_preview.rb
@@ -1,6 +1,6 @@
 # Preview all emails at http://localhost:3000/rails/mailers/portal_mailer
 class PortalMailerPreview < ActionMailer::Preview
   def generic_email_preview
-    PortalMailer.generic_email(true, "receiver@example.com", "reply@to.me", "This is subject", "This is content")
+    PortalMailer.generic_email("receiver@example.com", "reply@to.me", "This is subject", "This is content")
   end
 end
---
+++ spec/services/mailer_spec.rb
@@ -0,0 +1,53 @@
+require "rails_helper"
+
+RSpec.describe Mailer, type: :service do
+  let(:recipients_string) { 'test@example.de, test2@example.de' }
+  let(:recipients_array) { recipients_string.lines(',') }
+  let(:reply_to) { ['test3@example.de'] }
+  let(:subject) {'Subject'}
+  let(:content) {'Awesome content'}
+
+  describe 'mail sending with direct addressing' do
+    it 'sends right amount of emails with an array of recipients given' do
+      expect{Mailer.send_generic_email(false, recipients_array, reply_to, subject, content)}.to change{ActionMailer::Base.deliveries.count}.by(1)
+    end
+
+    it 'sends right amount of emails with a string of recipients given' do
+      expect{Mailer.send_generic_email(false, recipients_string, reply_to, subject, content)}.to change{ActionMailer::Base.deliveries.count}.by(1)
+    end
+
+    it 'has the correct number of recipients with an array of recipients given' do
+      Mailer.send_generic_email(false, recipients_array, reply_to, subject, content)
+      expect(ActionMailer::Base.deliveries.last.to.count).to eq(recipients_array.count)
+    end
+
+    it 'has the correct number of recipients with a string of recipients given' do
+      Mailer.send_generic_email(false, recipients_string, reply_to, subject, content)
+      expect(ActionMailer::Base.deliveries.last.to.count).to eq(recipients_array.count)
+    end
+  end
+
+  describe 'mail sending with indirect addressing' do
+    it 'sends right amount of emails with an array of recipients given' do
+      expect{Mailer.send_generic_email(true, recipients_array, reply_to, subject, content)}.to change{ActionMailer::Base.deliveries.count}.by(recipients_array.count)
+    end
+
+    it 'sends right amount of emails with a string of recipients given' do
+      expect{Mailer.send_generic_email(true, recipients_string, reply_to, subject, content)}.to change{ActionMailer::Base.deliveries.count}.by(recipients_array.count)
+    end
+
+    it 'hides recipients from each other with an array of recipients given' do
+      Mailer.send_generic_email(true, recipients_array, reply_to, subject, content)
+      ActionMailer::Base.deliveries.last(recipients_array.count).each do |delivery|
+        expect(delivery.to.count).to eq(1)
+      end
+    end
+
+    it 'hides recipients from each other with a string of recipients given' do
+      Mailer.send_generic_email(true, recipients_string, reply_to, subject, content)
+      ActionMailer::Base.deliveries.last(recipients_array.count).each do |delivery|
+        expect(delivery.to.count).to eq(1)
+      end
+    end
+  end
+end
