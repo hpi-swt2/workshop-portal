e54ad8a2e9f4e165f645f6ba15edd5f2c500852e
Cornelius Pohl <pohl.cornelius@gmail.com>
2016-12-14 08:39:34 +0100

    Add corrput PDF check - uploaded agreement letter

--- app/models/agreement_letter.rb
+++ app/models/agreement_letter.rb
@@ -9,6 +9,8 @@
 #  created_at  :datetime         not null
 #  updated_at  :datetime         not null
 #
+require "pdf/inspector"
+
 class AgreementLetter < ActiveRecord::Base
   belongs_to :user
   belongs_to :event
@@ -74,6 +76,9 @@ class AgreementLetter < ActiveRecord::Base
       elsif wrong_filetype?(file)
         errors.add(:file, I18n.t("agreement_letters.wrong_filetype"))
         false
+      elsif wrong_file_trailer?(file)
+        errors.add(:file, I18n.t("agreement_letters.corrupt_document"))
+        false
       else
         true
       end
@@ -90,4 +95,13 @@ class AgreementLetter < ActiveRecord::Base
     def wrong_filetype?(file)
       file.content_type != ALLOWED_MIMETYPE
     end
+
+  def wrong_file_trailer? file
+    begin
+    page_analysis = PDF::Inspector::Page.analyze(file.open)
+    false
+    rescue
+      true
+    end
+  end
 end
