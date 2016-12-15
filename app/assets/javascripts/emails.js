2f74e9748ca6699a205e5d6c21f3a0e0e65e8775
Tim Basel <tim.basel@student.hpi.de>
2016-12-15 09:46:24 +0100

    Fix for not registered Button Click

--- app/assets/javascripts/emails.js
+++ app/assets/javascripts/emails.js
@@ -1,2 +1,14 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+jQuery(function() {
+    $(document).on('click', "#send-emails-clipboard", function() {
+        console.log('Test');
+        $("#email_recipients").select();
+        try {
+           document.execCommand('copy');
+        } catch (err) {
+            console.log('Unable to copy emails to the clipboard');
+        }
+    });
+});
