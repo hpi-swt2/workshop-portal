4cbdf7048a3c36aaa2a7d9f7c6de04431123bd72
Bjarne Sievers <jellyfishcoding@live.de>
2016-12-15 19:33:43 +0100

    Add test for adding sample data to database

--- spec/db/seeds_spec.rb
+++ spec/db/seeds_spec.rb
@@ -1,4 +1,5 @@
 require 'rails_helper'
+require 'rake'
 
 describe 'Seeds' do
   it 'should seed the database without errors' do
@@ -6,5 +7,12 @@ describe 'Seeds' do
       # Try to seed the databse. Same as executing db:seed.
       Rails.application.load_seed
     }.to_not raise_error
+  end
+end
+
+describe 'Sample data' do
+  it 'should load sample data without errors' do
+    Rails.application.load_tasks
+    Rake::Task['db:populate_sample_data'].invoke
   end
 end
