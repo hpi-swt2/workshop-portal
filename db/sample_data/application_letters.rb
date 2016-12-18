faf6c3f9dae0cbbb00c9c312ecf0e9d8aad43694
Nils Strelow <nils.strelow@student.hpi.de>
2016-12-18 18:26:13 +0100

    add sample data for all application statuses

--- db/sample_data/application_letters.rb
+++ db/sample_data/application_letters.rb
@@ -14,6 +14,40 @@ def application_letter_applicant_gongakrobatik(user, event)
   )
 end
 
+def application_letter_applicant_gongakrobatik_rejected(user, event)
+  ApplicationLetter.new(
+      motivation: "Ich habe vor kurzem davon erfahren und war direkt hellaufbegeistert. Gerne würde ich mich bei Ihnen näher über das Thema informieren",
+      grade: 10,
+      experience: "Über einen Facebookpost ihrer Seite bin ich auf das Angebot aufmerksam geworden",
+      coding_skills: "HTML",
+      emergency_number: "01234567891",
+      vegeterian: false,
+      vegan: false,
+      allergic: false,
+      allergies: "",
+      user: user,
+      event: event,
+      status: ApplicationLetter.statuses[:rejected]
+  )
+end
+
+def application_letter_applicant_gongakrobatik_accepted(user, event)
+  ApplicationLetter.new(
+      motivation: "Den normalen Unterricht in der Schule finde ich ziemlich langweilig und würde mich darüber freuen, etwas über den Tellerrand zu schauen und spannende Dinge lernen. Ich arbeite sehr gerne im Team und freue mich darauf, Gleichgesinnte kennen zu lernen.",
+      grade: 9,
+      experience: "Über einen Zeitungsartikel",
+      coding_skills: "For, While und If-Schleifen in Java",
+      emergency_number: "01234567891",
+      vegeterian: false,
+      vegan: false,
+      allergic: true,
+      allergies: "Tomaten",
+      user: user,
+      event: event,
+      status: ApplicationLetter.statuses[:accepted]
+  )
+end
+
 def application_letter_applicant_programmierkurs_1(user, event)
   ApplicationLetter.new(
     motivation: "Den normalen Unterricht in der Schule finde ich ziemlich langweilig und würde mich darüber freuen, etwas über den Tellerrand zu schauen und spannende Dinge lernen. Ich arbeite sehr gerne im Team und freue mich darauf, Gleichgesinnte kennen zu lernen.",
