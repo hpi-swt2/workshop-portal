fb997952c71f1da2afadba593c98061e149baf49
Philipp <philipp.bode@student.hpi.de>
2017-01-03 14:41:49 +0100

    user is now redirected to profile creation page upon sign up. #195

--- config/routes.rb
+++ config/routes.rb
@@ -17,7 +17,7 @@ Rails.application.routes.draw do
     post 'badges' => 'events#print_badges', as: :print_badges
   end
   resources :profiles
-  devise_for :users
+  devise_for :users, :controllers => {:registrations => "users/registrations"}
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
