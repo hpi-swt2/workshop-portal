1a4d74f851817ba147e4953766c5c3bb08193be5
Eva <drache555@gmx.de>
2017-01-04 19:00:34 +0100

    revert version number

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161211123737) do
+ActiveRecord::Schema.define(version: 20161209180054) do
 
   create_table "agreement_letters", force: :cascade do |t|
     t.integer  "user_id",    null: false
