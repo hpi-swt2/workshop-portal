a8b215aaf345d00908a5bcc30e5f6613e8ef39a4
Alexander Löser <alexander.loeser@student.hpi.uni-potsdam.de>
2017-01-04 15:04:28 +0100

    Abilities for #185

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -45,13 +45,13 @@ class Ability
     end
     if user.role? :coach
       # Coaches can view Applications and participants for Event
-      can [:view_applicants, :view_participants], Event
+      can [:view_applicants, :view_participants, :print_applications], Event
       can [:view_and_add_notes, :show], ApplicationLetter
-      can [:print_applications], Event
+      cannot [:check], ApplicationLetter
     end
     if user.role? :organizer
       can [:index, :show], Profile
-      can [:index, :show, :view_and_add_notes, :update_status, :check], ApplicationLetter
+      can [:index, :show, :view_and_add_notes, :update_status], ApplicationLetter
       cannot :update, ApplicationLetter
       # Organizers can view, edit and print Applications and view participants for Events
       can [:view_applicants, :edit_applicants, :view_participants, :print_applications], Event
