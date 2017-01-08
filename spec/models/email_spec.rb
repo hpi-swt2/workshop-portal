9c9855da1c5bde19e7be7887ec2131450a6c30ab
Tim Basel <tim.basel@student.hpi.de>
2017-01-08 11:54:18 +0100

    removed test of functionality that is now handled by rails itself

--- spec/models/email_spec.rb
+++ spec/models/email_spec.rb
@@ -3,7 +3,6 @@ require 'rails_helper'
 describe Email do
   let(:valid_email) {FactoryGirl.build(:email) }
   let(:invalid_email) { FactoryGirl.build(:email, content: nil, subject: nil) }
-  let(:multiline_email) { FactoryGirl.build(:email, content: "Email-content \n Email-Content") }
 
   it 'is build by factory' do
     expect(valid_email).to be_valid
@@ -11,9 +10,5 @@ describe Email do
 
   it 'should not be valid without content' do
     expect(invalid_email).to_not be_valid
-  end
-
-  it 'should convert ruby line breaks to html <br>' do
-    expect(multiline_email.content).to_not include('\n')
   end
 end
