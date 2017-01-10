453516e6a10dcf8c458bde82469a924dc300509d
eva.krebs <eva.krebs@student.hpi.de>
2017-01-10 12:18:25 +0100

    remove unnecessary comment

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -182,7 +182,6 @@ describe Event do
 
   it "accepts all its application letters" do
     event = FactoryGirl.create :event, :with_diverse_open_applications
-    #TODO set max_participants to infinite, when participant number becomes relevant (eg @event.max_participants = event.infty)
     event.accept_all_application_letters
     application_letters = ApplicationLetter.where(event: event.id)
     expect(application_letters.all? { |application_letter| application_letter.status == 'accepted' }).to eq(true)
