c9a1b64486b755e151a76ceb0478b662f89927f9
Sebastian Bischoff <sebastian.bischoff@student.hpi.de>
2017-01-10 22:39:44 +0100

    Add tests for #125

--- spec/views/application_letters/show.html.erb_spec.rb
+++ spec/views/application_letters/show.html.erb_spec.rb
@@ -17,9 +17,33 @@ RSpec.describe "application_letters/show", type: :view do
     expect(rendered).to have_text(@application_letter.user.profile.name)
     expect(rendered).to have_text(@application_letter.user.profile.gender)
     expect(rendered).to have_text(@application_letter.user.profile.age_at_time(@application_letter.event.start_date))
-    expect(rendered).to have_text(@application_letter.user.profile.address)
     expect(rendered).to have_text(@application_letter.user.accepted_applications_count(@application_letter.event))
     expect(rendered).to have_text(@application_letter.user.rejected_applications_count(@application_letter.event))
   end
 
+  %i[coach organizer].each do |role|
+    it "logged in as #{role} I cannot see personal details" do
+      login(role)
+      expect(rendered).to_not have_text(@application_letter.user.profile.address)
+      expect(rendered).to_not have_text(Profile.human_attribute_name(:school))
+    end
+  end
+
+  it "logged in as admin I can see personal details" do
+    login(:admin)
+    expect(rendered).to have_text(@application_letter.user.profile.address)
+  end
+
+  %i[organizer admin].each do |role|
+    it "logged in as #{role} I can click on the applicants name" do
+      login(role)
+      expect(rendered).to have_link(@application_letter.user.profile.name, :href => profile_path(@application_letter.user))
+    end
+  end
+
+  def login(role)
+    @profile = FactoryGirl.create(:profile)
+    @profile.user.role = role
+    login_as(@profile.user, :scope => :user)
+  end
 end
