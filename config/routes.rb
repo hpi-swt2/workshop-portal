48418a55dba13ec3e101caa2c8e28c1193ad9491
Christian Flach <cmfcmf@users.noreply.github.com>
2017-01-11 11:03:43 +0100

    Completely remove Profile#delete action. (#295)

--- config/routes.rb
+++ config/routes.rb
@@ -18,7 +18,7 @@ Rails.application.routes.draw do
     post 'badges' => 'events#print_badges', as: :print_badges
     post 'upload_material' => 'events#upload_material', as: :upload_material
   end
-  resources :profiles
+  resources :profiles, except: [:index, :destroy]
   devise_for :users, :controllers => {:registrations => "users/registrations"}
   resources :users, only: [:index] # index page for devise users
   patch 'users/:id/role' => 'users#update_role', as: :update_user_role
