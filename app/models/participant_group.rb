4d61686acd2e51334fbbaeb65429e21a9d627b36
Lennart L <lennart.lehmann@student.hpi.de>
2017-01-11 04:13:38 +0100

    add cross icon to no group color selector

--- app/models/participant_group.rb
+++ app/models/participant_group.rb
@@ -12,7 +12,7 @@ class ParticipantGroup < ActiveRecord::Base
 
   belongs_to :application_letter
 
-  GROUPS = { 0 => 'none', 1 => 'darkblue', 2 => 'red', 3 => 'yellow', 4 => 'orange', 5 => 'lightgreen', 6 => 'purple', 7 => 'deeppink', 8 => 'darkgreen', 9 => 'lightblue', 10 => 'gray' }
+  GROUPS = { 0 => 'none', 1 => 'darkblue', 2 => 'red', 3 => 'yellow', 4 => 'orange', 5 => 'lightgreen', 6 => 'purple', 7 => 'deeppink', 8 => 'darkgreen', 9 => 'lightblue', 10 => 'black' }
   GROUPS.default = 0
 
   validates_inclusion_of :group, :in => GROUPS.keys
