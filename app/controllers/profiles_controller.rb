b64659948653ad55ce74cf4cee36cd528cf55d39
Alec Schneider <alecschneider@me.com>
2017-01-13 14:10:01 +0100

    Removed and added things #245

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -26,7 +26,7 @@ class ProfilesController < ApplicationController
       if flash[:event_id]
         redirect_to new_application_letter_path(:event_id => flash[:event_id]), notice: I18n.t('profiles.successful_creation')
       else
-        redirect_to @profile, notice: I18n.t('profiles.successful_creation')
+        redirect_to edit_user_registration_path, notice: I18n.t('profiles.successful_creation')
       end
     else
       render :new
@@ -36,7 +36,7 @@ class ProfilesController < ApplicationController
   # PATCH/PUT /profiles/1
   def update
     if @profile.update(profile_params)
-      redirect_to @profile, notice: I18n.t('profiles.successful_update')
+      redirect_to edit_user_registration_path, notice: I18n.t('profiles.successful_update')
     else
       render :edit
     end
