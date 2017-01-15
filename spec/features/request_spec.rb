hpi-swt2/workshop-portal

commit b4c1b10a9c97cbde77082e14091d88135c0b7f93

Display status translation

app/views/requests/show.html.erb
@@ -13,7 +13,7 @@
   <dt><strong><%= model_class.human_attribute_name(:number_of_participants) %>:</strong></dt>
   <dd><%= @request.number_of_participants %></dd>
   <dt><strong><%= model_class.human_attribute_name(:status) %>:</strong></dt>
-  <dd><%= @request.status %></dd>
+  <dd><%= t(@request.status, scope: 'activerecord.attributes.request.statuses') %></dd>
 </dl>
 
 <%= link_to t('.back', :default => t("helpers.links.back")),

commit 0a972fb2a5559826417612be93818782f71ba1ad

Request detail page (#330)
* Add status to requests

* Failing test for #219

* Add feature spec

* Change view for Request :show

* Add controller action #accept

* Add tests for controller action

* Fix tests

* Move the accept button to actions not to disrupt layout

* Fix test (wrong comparison string - symbol)

* Remove failing controller test (content covered in feature spec)

Implementing authentication in controller tests seems out of scope for now

* Display status translation

* Fix translated status not being recognized

Caused by b4c1b10a9c97cbde77082e14091d88135c0b7f93

* Simplify controller

Using 'bang!' save method

* Remove model access in feature test

* Whitespace changes after migration

app/controllers/requests_controller.rb
@@ -1,5 +1,5 @@
 class RequestsController < ApplicationController
-  before_action :set_request, only: [:show, :edit, :update, :destroy]
+  before_action :set_request, only: [:show, :edit, :update, :destroy, :accept]
 
   # GET /requests
   def index
@@ -44,6 +44,13 @@ class RequestsController < ApplicationController
   def destroy
     @request.destroy
     redirect_to requests_url, notice: 'Request was successfully destroyed.'
+  end
+
+  def accept
+    authorize! :change_status, @request
+    @request.status = :accepted
+    @request.save!
+    redirect_to @request, notice: I18n.t('requests.notice.was_accepted')
   end
 
   private

app/models/request.rb
@@ -11,8 +11,9 @@ class Request < ActiveRecord::Base
   validates_presence_of :form_of_address, :last_name, :first_name, :phone_number, :street, :zip_code_city, :email, :topic_of_workshop
   validates :number_of_participants, numericality: { only_integer: true, greater_than: 0 }, allow_nil: :true
   validates_format_of :email, :with => Devise::email_regexp
-  
+
   enum form_of_address: [:mr, :mrs, :prefer_to_omit]
+  enum status: [:open, :accepted]  # per database declaration, the first value is default
 
   def name
     "#{first_name} #{last_name}"

app/views/requests/show.html.erb
@@ -10,6 +10,10 @@
   <dd><%= @request.last_name %></dd>
   <dt><strong><%= model_class.human_attribute_name(:topic_of_workshop) %>:</strong></dt>
   <dd><%= @request.topic_of_workshop %></dd>
+  <dt><strong><%= model_class.human_attribute_name(:number_of_participants) %>:</strong></dt>
+  <dd><%= @request.number_of_participants %></dd>
+  <dt><strong><%= model_class.human_attribute_name(:status) %>:</strong></dt>
+  <dd><%= t(@request.status, scope: 'activerecord.attributes.request.statuses') %></dd>
 </dl>
 
 <%= link_to t('.back', :default => t("helpers.links.back")),
@@ -17,6 +21,10 @@
 <% if can? :edit, @request %>
   <%= link_to t('.edit', :default => t("helpers.links.edit")),
               edit_request_path(@request), :class => 'btn btn-default' %>
+  <% if @request.open? %>
+    <%= link_to t('requests.form.accept'),
+              accept_request_path(@request.id), :class => 'btn btn-success' %>
+  <% end %>
 <% end %>
 <% if can? :destroy, @request %>
   <%= link_to t('.destroy', :default => t("helpers.links.destroy")),

config/locales/de.requests.yml
@@ -7,9 +7,12 @@ de:
       mrs: "Frau"
       prefer_to_omit: "Nicht angeben"
     form:
-      create_request: "Anfrage erstellen" 
+      create_request: "Anfrage erstellen"
+      accept: "Annehmen"
     hints:
       please_enter_two_topics: "Bitte 2 Wünsche angeben"
+    notice:
+      was_accepted: "Die Anfrage wurde angenommen"
   activerecord:
     models:
       request:
@@ -24,11 +27,15 @@ de:
         zip_code_city: "PLZ und Stadt"
         email: "E-Mail-Adresse"
         topic_of_workshop: "Thema des Workshops"
-        time_period: "Zeitrahmen" 
+        time_period: "Zeitrahmen"
         number_of_participants: "Teilnehmeranzahl"
         knowledge_level: "Kenntnisstand"
         annotations: "Anmerkungen"
         id: "Nummer"
         created_at: "Erstellt am"
+        status: "Status"
+        statuses:
+          open: "Noch nicht angenommen"
+          accepted: "Angenommen"
   dialogues:
     confirm_delete: "Bist du sicher, dass du diese Anfrage löschen möchtest? Diese Aktion kann nicht widerrufen werden."

config/routes.rb
@@ -38,6 +38,12 @@ Rails.application.routes.draw do
 
   post 'send_email' => 'emails#send_email'
 
+  resources :requests do
+    member do
+      get :accept
+    end
+  end
+
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 

db/migrate/20170108122417_add_status_to_requests.rb
@@ -1,0 +1,5 @@
+class AddStatusToRequests < ActiveRecord::Migration
+  def change
+    add_column :requests, :status, :integer, default: 0
+  end
+end

db/sample_data/requests.rb
@@ -8,6 +8,7 @@ def request_hardware_entwicklung
     zip_code_city: "12345 Musterstadt",
     email: "mustermann@example.de",
     topic_of_workshop: "Hardware-Entwicklung mit einem CAD-System",
-    number_of_participants: 12
+    number_of_participants: 12,
+    status: :open
   )
 end

db/schema.rb
@@ -98,8 +98,8 @@ ActiveRecord::Schema.define(version: 20170114132153) do
   add_index "profiles", ["user_id"], name: "index_profiles_on_user_id"
 
   create_table "requests", force: :cascade do |t|
-    t.datetime "created_at",             null: false
-    t.datetime "updated_at",             null: false
+    t.datetime "created_at",                         null: false
+    t.datetime "updated_at",                         null: false
     t.integer  "form_of_address"
     t.string   "first_name"
     t.string   "last_name"
@@ -111,6 +111,7 @@ ActiveRecord::Schema.define(version: 20170114132153) do
     t.integer  "number_of_participants"
     t.string   "knowledge_level"
     t.text     "annotations"
+    t.integer  "status",                 default: 0
     t.string   "zip_code_city"
   end
 

spec/controllers/requests_controller_spec.rb
@@ -63,6 +63,14 @@ RSpec.describe RequestsController, type: :controller do
       end
     end
 
+    describe "GET #accept" do
+      it "rejects the request for normal users" do
+        get :accept, id: @a_request.to_param, session: valid_session
+        expect(@a_request.status.to_sym).to eq(:open)
+      end
+    end
+
+
     describe "PUT #update" do
       context "with valid params" do
         let(:new_attributes) {

spec/factories/requests.rb
@@ -18,5 +18,6 @@ FactoryGirl.define do
     email "mustermann@example.de"
     topic_of_workshop "Hardware-Entwicklung mit einem CAD-System"
     number_of_participants 12
+    status :open
   end
 end

spec/features/request_spec.rb
@@ -59,4 +59,26 @@ describe "workshop requests", type: :feature do
       expect(page).to have_text(@request.topic_of_workshop)
     end
   end
+
+  describe "show page" do
+    context "as an organizer" do
+      before(:each) do
+        profile = FactoryGirl.create(:profile)
+        organizer = FactoryGirl.create(:user, role: :organizer, profile: profile)
+        login_as(organizer, scope: :user)
+      end
+
+      it "should allow me to change the status to :accepted and display the change" do
+        request = FactoryGirl.create(:request, status: :open)
+        visit(request_path(request))
+        status = I18n.t(@request.status, scope: 'activerecord.attributes.request.statuses')
+        expect(page).to have_text(status)
+        click_link I18n.t('requests.form.accept')
+        expect(page).to have_text(I18n.t('requests.notice.was_accepted'))
+        visit(request_path(request))
+        accepted_status = I18n.t 'activerecord.attributes.request.statuses.accepted'
+        expect(page).to have_text(accepted_status)
+      end
+    end
+  end
 end

spec/views/requests/show.html.erb_spec.rb
@@ -2,12 +2,16 @@ require 'rails_helper'
 
 RSpec.describe "requests/show", type: :view do
   before(:each) do
-    @aRequest = assign(:request, FactoryGirl.create(:request, topic_of_workshop: 'Topics'))
+    @aRequest = assign(:request, FactoryGirl.create(:request, topic_of_workshop: 'Topics', status: :open))
   end
 
   it "renders attributes" do
     render
     expect(rendered).to have_text(@aRequest.topic_of_workshop)
+    expect(rendered).to have_text(@aRequest.first_name)
+    expect(rendered).to have_text(@aRequest.last_name)
+    expect(rendered).to have_text(@aRequest.number_of_participants)
+    expect(rendered).to have_text(I18n.t(@aRequest.status, scope: 'activerecord.attributes.request.statuses'))
   end
 
   it "should not display edit, delete buttons for non-organizers" do
@@ -23,6 +27,12 @@ RSpec.describe "requests/show", type: :view do
       render
       expect(rendered).to have_link(I18n.t('helpers.links.edit'))
       expect(rendered).to have_link(I18n.t('helpers.links.destroy'))
+      expect(rendered).to have_link(I18n.t('requests.form.accept'))
     end
+  end
+
+  it "doesn't show the accept button for accepted events" do
+    @aRequest = assign(:request, FactoryGirl.create(:request, topic_of_workshop: 'Topics', status: :accepted))
+    expect(rendered).not_to have_link(I18n.t('helpers.links.accept'))
   end
 end