d495bd670386f4d7c5f8ab7925936f44e9cfe50a
Fabian Sommer <fabiansbg@web.de>
2017-01-15 15:12:00 +0100

    Reversing values of draft when migrating

--- db/migrate/20170111135212_rename_draft_to_published.rb
+++ db/migrate/20170111135212_rename_draft_to_published.rb
@@ -1,5 +1,20 @@
 class RenameDraftToPublished < ActiveRecord::Migration
   def change
+    reversible do |dir|
+      dir.up do
+        execute <<-SQL
+          UPDATE 'events'
+          SET draft = CASE draft WHEN 't' THEN 'f' ELSE 't' END;
+        SQL
+      end
+      dir.down do
+        execute <<-SQL
+          UPDATE 'events'
+          SET draft = CASE draft WHEN 't' THEN 'f' ELSE 't' END;
+        SQL
+      end
+    end
+
     rename_column :events, :draft, :published
   end
 end
