193af4a522bc69870c611df1a5963ccb7177d258
Carl Gödecken <carl.goedecken@student.hpi.de>
2017-01-16 15:17:46 +0100

    View specs for #321

--- spec/views/application_letters/new.html.erb_spec.rb
+++ spec/views/application_letters/new.html.erb_spec.rb
@@ -2,7 +2,8 @@ require 'rails_helper'
 
 RSpec.describe "application_letters/new", type: :view do
   before(:each) do
-    assign(:application_letter, FactoryGirl.build(:application_letter))
+    @application_letter = FactoryGirl.build(:application_letter)
+    assign(:application_letter, @application_letter)
   end
 
   it "renders new application form" do
@@ -18,6 +19,11 @@ RSpec.describe "application_letters/new", type: :view do
       assert_select "input#application_letter_allergic[name=?]", "application_letter[allergic]"
       assert_select "textarea#application_letter_allergies[name=?]", "application_letter[allergies]"
       assert_select "select#application_letter_grade[name=?]", "application_letter[grade]"
+      @application_letter.event.custom_application_fields.each { |field_name|
+        assert_select "label.control-label[for=custom_application_fields_]", field_name
+      }
+      assert_select "input#custom_application_fields_", count: @application_letter.event.custom_application_fields.count
     end
   end
+
 end
