b7c83e1079140bbae6120df71a6fc58c2e495ca3
Matthias Radscheit <matthias@radscheit.de>
2017-01-19 09:28:18 +0100

    schema

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 # It's strongly recommended that you check this file into your version control system.
 
 
-ActiveRecord::Schema.define(version: 20170114132153) do
+ActiveRecord::Schema.define(version: 20170118185870) do
 
 
   create_table "agreement_letters", force: :cascade do |t|
