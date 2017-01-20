dddc9a3960a5453fcad4b5c9933fb15c00cba7d6
Tom Beckmann <tomjonabc@gmail.com>
2017-01-20 17:45:10 +0100

    Use activerecord queries instead of sql

--- db/migrate/20170111135212_rename_draft_to_published.rb
+++ db/migrate/20170111135212_rename_draft_to_published.rb
@@ -2,16 +2,10 @@ class RenameDraftToPublished < ActiveRecord::Migration
   def change
     reversible do |dir|
       dir.up do
-        execute <<-SQL
-          UPDATE 'events'
-          SET draft = CASE draft WHEN 't' THEN 'f' ELSE 't' END;
-        SQL
+        Event.all.each {|e| e.toggle!(:draft)}
       end
       dir.down do
-        execute <<-SQL
-          UPDATE 'events'
-          SET draft = CASE draft WHEN 't' THEN 'f' ELSE 't' END;
-        SQL
+        Event.all.each {|e| e.toggle!(:published)}
       end
     end
 
