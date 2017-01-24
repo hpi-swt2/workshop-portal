dd7b9a41f1fac65761b0e53d10a25a75b757f8c1
JuliusCosmoRomeo <julius.rudolph@student.hpi.de>
2017-01-24 18:19:59 +0100

    merged dev and fixed broken test

--- db/schema.rb
+++ db/schema.rb
@@ -77,10 +77,10 @@ ActiveRecord::Schema.define(version: 20170123161527) do
     t.integer  "max_participants"
     t.datetime "created_at",                                 null: false
     t.datetime "updated_at",                                 null: false
-    t.string   "organizer"
-    t.string   "knowledge_level"
     t.integer  "kind",                       default: 0
     t.boolean  "published"
+    t.string   "organizer"
+    t.string   "knowledge_level"
     t.date     "application_deadline"
     t.boolean  "application_status_locked"
     t.boolean  "participants_are_unlimited", default: false
--- spec/features/user_spec.rb
+++ spec/features/user_spec.rb
@@ -8,11 +8,11 @@ RSpec.feature "Account creation", :type => :feature do
 
     password = "mybirthdate"
     # http://www.rubydoc.info/github/jnicklas/capybara/Capybara/Node/Actions:fill_in
-    fill_in "user_email", :with => "first.last@example.com"
-    fill_in "user_password", :with => password
-    fill_in "user_password_confirmation", :with => password
+    fill_in "sign_up_email", :with => "first.last@example.com"
+    fill_in "sign_up_password", :with => password
+    fill_in "sign_up_password_confirmation", :with => password
     # http://www.rubydoc.info/github/jnicklas/capybara/Capybara%2FNode%2FFinders%3Afind
-    find('input[name="commit"]').click
+    find('input[id="sign_up_submit"]').click
 
     # Show success alert
     # http://www.rubydoc.info/github/jnicklas/capybara/master/Capybara/RSpecMatchers#have_css-instance_method
