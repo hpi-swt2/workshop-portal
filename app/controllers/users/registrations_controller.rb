b6362b0554de036312461478e471a4c70df57c5c
JuliusCosmoRomeo <julius.rudolph@student.hpi.de>
2017-01-24 18:03:06 +0100

    tried solving registration fail errors

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -8,9 +8,29 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # end
 
   # POST /resource
-  # def create
-  #   super
-  # end
+  def create
+    build_resource(sign_up_params)
+
+    resource.save
+    yield resource if block_given?
+    if resource.persisted?
+      if resource.active_for_authentication?
+        set_flash_message! :notice, :signed_up
+        sign_up(resource_name, resource)
+        respond_with resource, location: after_sign_up_path_for(resource)
+      else
+        set_flash_message! :notice, :"signed_up_but_#{resource.inactive_message}"
+        expire_data_after_sign_in!
+        respond_with resource, location: after_inactive_sign_up_path_for(resource)
+      end
+    else
+      clean_up_passwords resource
+      set_minimum_password_length
+      resource.errors.full_messages.each {|x| flash[x] = x} # Rails 4 simple way
+      respond_with resource
+      #respond_with_navigational(resource) { render_with_scope :new }
+    end
+  end
 
   # GET /resource/edit
   # def edit
