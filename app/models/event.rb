c600f40459854ca16177aa9b87868e6950ca4d4a
Nils Thamm <nils.thamm@student.hpi.de>
2017-01-24 12:02:55 +0100

    Fixing view tests

--- app/models/event.rb
+++ app/models/event.rb
@@ -279,7 +279,7 @@ class Event < ActiveRecord::Base
   # @param only_public Set to true to not include drafts
   # @return List of events
   def self.sorted_by_start_date(only_public)
-    (only_public ? Event.draft_is(false) : Event.all)
+    (only_public ? Event.draft_is(false).where(hidden: false) : Event.all)
       .sort_by(&:start_date)
   end
 
