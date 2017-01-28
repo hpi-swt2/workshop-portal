f9b5b513674abd2e94811ae151a529b00c96fa70
Matthias Radscheit <matthias@radscheit.de>
2017-01-28 17:02:42 +0100

    Several Fixes

--- app/assets/javascripts/requests.js
+++ app/assets/javascripts/requests.js
@@ -1,13 +1,18 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 
-jQuery(function($) {
-	$("#requestGallery").owlCarousel({
+function enableOwlCarousel() {
+	$(".owl-carousel").owlCarousel({
 		center: false,
 		item: 2,
 		autoplay: true,
 		loop: true,
 		dots: true,
 		margin: 30,
+		nav: true,
 	});
-})
+}
+
+jQuery(document).on('turbolinks:partical-load', enableOwlCarousel);
+jQuery(enableOwlCarousel);
+
