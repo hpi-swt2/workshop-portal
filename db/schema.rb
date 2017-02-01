4ceda367efa05e8ae097a2b5b45f97f9670f2367
Alexander Riese <alexander.riese@student.hpi.de>
2017-02-01 16:09:56 +0100

    Refs: #399: Add missing migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170131092522) do
+ActiveRecord::Schema.define(version: 20170201150603) do
 
   create_table "agreement_letters", force: :cascade do |t|
     t.integer  "user_id",    null: false
