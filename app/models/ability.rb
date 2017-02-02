29e4b58aff1719d76a0340afa070a6fbe5135b23
Alec Schneider <alecschneider@me.com>
2017-02-02 17:30:41 +0100

    fix this fuckin test from merge for #389

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -69,7 +69,8 @@ class Ability
       can :send_email, Email
       can [:manage, :set_contact_person, :set_notes], Request
       cannot :apply, Event
-      can [:view_apply_button, :edit, :update], Event
+      cannot :view_apply_button, Event
+      can [:edit, :update], Event
       can [:update], ParticipantGroup
 
       # Organizers can update user roles of pupil, coach and organizer, but cannot manage admins and cannot update a role to admin
