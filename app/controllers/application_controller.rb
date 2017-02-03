73840cce08ff3d5fbdbc633723cb8f990dbdb3f5
Philipp <philipp.bode@student.hpi.de>
2017-02-03 16:36:45 +0100

    Refactored application index to use scopes instead of custom selections

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,8 +19,8 @@ class ApplicationController < ActionController::Base
     #       we should if this becomes a perf problem), there is no
     #       other solution
 
-    @events = Event.sorted_by_start_date(true)
-      .select { |a| a.start_date > Time.now }
+    @events = Event.future
+      .draft_is(false).hidden_is(false)
       .first(3)
     render 'index', locals: { full_width: true }
   end
