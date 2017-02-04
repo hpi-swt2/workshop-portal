93bf8d4d6b72b6e031d37c88b5a4bfee2251730e
Sebastian Bischoff <sebastian.bischoff@student.hpi.de>
2017-02-05 00:10:48 +0100

    Add tests for login

--- spec/features/interactions/standard_interaction_spec.rb
+++ spec/features/interactions/standard_interaction_spec.rb
@@ -11,6 +11,11 @@ RSpec::Steps.steps "Demo" do
     #TODO: Use id instead of visible string, currently id is missing for login button
     click_link I18n.t('navbar.login')
     login_with_credentials(user_organizer.email, user_password)
+    expect(page).to_not have_text I18n.t('devise.failure.inactive')
+    expect(page).to_not have_text I18n.t('devise.failure.invalid')
+    expect(page).to_not have_text I18n.t('devise.failure.not_found_in_database')
+    expect(page).to_not have_text I18n.t('devise.failure.locked')
+  end
   end
 end
 
