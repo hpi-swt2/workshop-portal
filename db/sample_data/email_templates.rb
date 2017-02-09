8a9c6679844eea8ac95947e3da7fde9285282ac0
Tobias Dürschmid <tobias.duerschmid@t-online.de>
2017-02-09 23:51:45 +0100

    Improve acceptance mail template

--- db/sample_data/email_templates.rb
+++ db/sample_data/email_templates.rb
@@ -5,20 +5,21 @@ def email_template_acceptance
     subject: "Zusage",
     content: 'Liebe Bewerberin / lieber Bewerber,
 
-zunächst einmal herzlichen Glückwunsch: **Du bist dabei!** Bitte lies Dir die nachfolgenden Informationen zum Camp aufmerksam durch. Sollte deine Teilnahme doch nicht möglich sein, bitten wir dich im Interesse der Schüler auf der Warteliste darum, uns so schnell wie möglich zu informieren.
-Terminliches und Telefonnummern
-Die Zeitpunkte für An- und Abreise sowie Telefonnummern für Probleme, z.B. größeren Zugverspätungen am Anreisetag findest Du auf dem angehängten Ablaufplan.
+zunächst einmal herzlichen Glückwunsch: **Du bist dabei!** Bitte lies Dir die nachfolgenden Informationen zum Camp aufmerksam durch. Sollte Deine Teilnahme doch nicht möglich sein, bitten wir Dich im Interesse der Schüler auf der Warteliste darum, uns so schnell wie möglich zu informieren.
+
+## Terminliches und Telefonnummern
+Die Zeitpunkte für An- und Abreise sowie Telefonnummern für Probleme, z.B. größeren Zugverspätungen am Anreisetag findest Du auf dem Ablaufplan, den wir Dir vor dem Camp zusenden werden.
 
 ## Einverständniserklärung
-Aus versicherungstechnischen Gründen musst Du, falls Du noch nicht volljährig bist, eine Einverständniserklärung deiner Eltern hochladen. Die Vorlage dafür findest Du im Anhang dieser E-Mail.
+Aus versicherungstechnischen Gründen musst Du, falls Du noch nicht volljährig bist, eine Einverständniserklärung Deiner Eltern hochladen. Die Vorlage dafür findest Du im Anhang dieser E-Mail.
 
 ## Wertsachen
 Wer Digitalkamera, Laptop, etc. mit zum Camp bringt, ist für deren Sicherheit selbst zuständig. Für Verluste kann das Hasso-Plattner-Institut leider nicht aufkommen.
 Weitere Informationen
 
-Aktuelle Information findest Du auch auf der Facebook-Seite des Schülerklubs unter: https://www.facebook.com/hpi.schuelerklub. Zudem haben wir eine Veranstaltung auf Facebook angelegt: https://www.facebook.com/events/846978382011807/. Dort kannst Du dich mit anderen Teilnehmern über Fahrgemeinschaften austauschen.
+Aktuelle Information findest Du auch auf der Facebook-Seite des Schülerklubs unter: https://www.facebook.com/hpi.schuelerklub. Zudem haben wir eine Veranstaltung auf Facebook angelegt: https://www.facebook.com/events/846978382011807/. Dort kannst Du Dich mit anderen Teilnehmern über Fahrgemeinschaften austauschen.
 
-Wir wünschen Dir eine schnelle und sichere Anreise. Bei Fragen kannst Du dich natürlich jederzeit an uns wenden.
+Wir wünschen Dir eine schnelle und sichere Anreise. Bei Fragen kannst Du Dich natürlich jederzeit an uns wenden.
 
 Viele Grüße aus Potsdam,
 Dein HPI-Workshop-Team
