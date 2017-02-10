32f922f908c64943ac35f58bccb0fb971f86952e
Tobias Dürschmid <tobias.duerschmid@t-online.de>
2017-02-10 22:15:02 +0100

    Revert "Merge pull request #616 from hpi-swt2/presentation_strings" (#624)
    This reverts commit 96b65f070f7dca89bde2f080a41328db5377a4a3, reversing
    changes made to 37347f4b753106948d0f25fedc523603cbb900a1.

--- db/sample_data/email_templates.rb
+++ db/sample_data/email_templates.rb
@@ -1,30 +1,29 @@
 def email_template_acceptance
   EmailTemplate.new(
     status: :acceptance,
-    hide_recipients: true,
-    subject: "Zusage",
-    content: 'Liebe Bewerberin / lieber Bewerber,
+    hide_recipients: false,
+    subject: "Zusage für TODO",
+    content: 'Liebe Camp-Teilnehmerinnen und -Teilnehmer,
 
-zunächst einmal herzlichen Glückwunsch: **Du bist dabei!** Bitte lies Dir die nachfolgenden Informationen zum Camp aufmerksam durch. Sollte Deine Teilnahme doch nicht möglich sein, bitten wir Dich im Interesse der Schüler auf der Warteliste darum, uns so schnell wie möglich zu informieren.
-
-## Terminliches und Telefonnummern
-Die Zeitpunkte für An- und Abreise sowie Telefonnummern für Probleme, z.B. größeren Zugverspätungen am Anreisetag findest Du auf dem Ablaufplan, den wir Dir vor dem Camp zusenden werden.
+zunächst einmal herzlichen Glückwunsch: **Du bist dabei!** Bitte lies Dir die nachfolgenden Informationen zum TODO, das vom TODO bis TODO stattfinden wird, aufmerksam durch. Sollte deine Teilnahme doch nicht möglich sein, bitten wir dich im Interesse der Schüler auf der Warteliste darum, uns so schnell wie möglich zu informieren.
+Terminliches und Telefonnummern
+Die Zeitpunkte für An- und Abreise sowie Telefonnummern für Probleme, z.B. größeren Zugverspätungen am Anreisetag findest Du auf dem angehängten Ablaufplan.
 
 ## Einverständniserklärung
-Aus versicherungstechnischen Gründen musst Du, falls Du noch nicht volljährig bist, eine Einverständniserklärung Deiner Eltern hochladen. Die Vorlage dafür findest Du im Anhang dieser E-Mail.
+Aus versicherungstechnischen Gründen musst Du, falls Du noch nicht volljährig bist, eine Einverständniserklärung deiner Eltern hochladen.
 
 ## Wertsachen
 Wer Digitalkamera, Laptop, etc. mit zum Camp bringt, ist für deren Sicherheit selbst zuständig. Für Verluste kann das Hasso-Plattner-Institut leider nicht aufkommen.
 Weitere Informationen
+Weitere Informationen werden nächste Woche direkt vom MINT-EC per Mail an euch versandt.
 
-Aktuelle Information findest Du auch auf der Facebook-Seite des Schülerklubs unter: https://www.facebook.com/hpi.schuelerklub. Zudem haben wir eine Veranstaltung auf Facebook angelegt: https://www.facebook.com/events/846978382011807/. Dort kannst Du Dich mit anderen Teilnehmern über Fahrgemeinschaften austauschen.
+Aktuelle Information findest Du auch auf der Facebook-Seite des Schülerklubs unter: https://www.facebook.com/hpi.schuelerklub. Zudem hat der MINT-EC eine Veranstaltung auf Facebook angelegt: https://www.facebook.com/events/846978382011807/
 
-Wir wünschen Dir eine schnelle und sichere Anreise. Bei Fragen kannst Du Dich natürlich jederzeit an uns wenden.
+Wir wünschen Dir eine schnelle und sichere Anreise. Bei Fragen kannst Du dich natürlich jederzeit an uns wenden.
 
 Viele Grüße aus Potsdam,
-Dein HPI-Workshop-Team
-
-_________________________________________________________
+dein HPI-Workshop-Team
+------------------------------------------------------------------------------
 Hasso-Plattner-Institut für Softwaresystemtechnik GmbH
 Prof.-Dr.-Helmert-Straße 2-3
 14482 Potsdam
@@ -32,7 +31,7 @@ Prof.-Dr.-Helmert-Straße 2-3
 www.hpi.de
 www.hpi.de/schueler
 
-Folge uns auch auf:
+Folgen Sie uns auch auf:
 www.facebook.com/hpi.schuelerklub
 www.facebook.com/HassoPlattnerInstitute
 www.twitter.com/HPI_Online
@@ -48,16 +47,16 @@ end
 def email_template_rejection
   EmailTemplate.new(
       status: :rejection,
-      hide_recipients: true,
-      subject: "Absage",
+      hide_recipients: false,
+      subject: "Absage für TODO",
       content: 'Liebe Bewerberin / lieber Bewerber,
 
-leider müssen wir Dir mitteilen, dass Du für das Camp nicht zugelassen wurdest.
+leider müssen wir Dir mitteilen, dass Du für TODO nicht zugelassen wurdest.
 Versuche es doch bei einem anderen unserer Camps.
 
 Viele Grüße aus Potsdam,
 dein HPI-Workshop-Team
-_________________________________________________________
+------------------------------------------------------------------------------
 Hasso-Plattner-Institut für Softwaresystemtechnik GmbH
 Prof.-Dr.-Helmert-Straße 2-3
 14482 Potsdam
@@ -65,7 +64,7 @@ Prof.-Dr.-Helmert-Straße 2-3
 www.hpi.de
 www.hpi.de/schueler
 
-Folge uns auch auf:
+Folgen Sie uns auch auf:
 www.facebook.com/hpi.schuelerklub
 www.facebook.com/HassoPlattnerInstitute
 www.twitter.com/HPI_Online
