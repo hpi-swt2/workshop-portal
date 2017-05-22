0fe5afead3d1aed87f67e448cfa5324d117206fe
Bjarne Sievers <jellyfishcoding@live.de>
2017-05-22 23:54:53 +0200

    Replace double quotes with single quotes where possible in all test files I touched

--- spec/views/events/edit.html.erb_spec.rb
+++ spec/views/events/edit.html.erb_spec.rb
@@ -1,14 +1,14 @@
 require 'rails_helper'
 
-RSpec.describe "events/edit", type: :view do
+RSpec.describe 'events/edit', type: :view do
   before(:each) do
     @event = assign(:event, FactoryGirl.create(:event))
   end
 
-  it "renders the edit event form" do
+  it 'renders the edit event form' do
     render
 
-    assert_select "form[action=?][method=?]", event_path(@event), "post" do
+    assert_select "form[action=?][method=?]", event_path(@event), 'post' do
       assert_select "input#event_name[name=?]", "event[name]"
       assert_select "textarea#event_description[name=?]", "event[description]"
       assert_select "input#event_max_participants[name=?]", "event[max_participants]"
@@ -17,13 +17,13 @@ RSpec.describe "events/edit", type: :view do
     end
   end
 
-  it "shows that organizer and knowledge_level are optional" do
+  it 'shows that organizer and knowledge_level are optional' do
     render
-    expect(rendered).to have_field("event_organizer", :placeholder => "optional")
-    expect(rendered).to have_field("event_knowledge_level", :placeholder => "optional")
+    expect(rendered).to have_field('event_organizer', :placeholder => 'optional')
+    expect(rendered).to have_field('event_knowledge_level', :placeholder => 'optional')
   end
 
-  it "should have a delete button" do
+  it 'should have a delete button' do
     @event = assign(:event, FactoryGirl.create(:event))
     render
     assert_select 'a[data-method="delete"]'
--- spec/views/events/new.html.erb_spec.rb
+++ spec/views/events/new.html.erb_spec.rb
@@ -1,14 +1,14 @@
 require 'rails_helper'
 
-RSpec.describe "events/new", type: :view do
+RSpec.describe 'events/new', type: :view do
   before(:each) do
     assign(:event, FactoryGirl.build(:event))
   end
 
-  it "renders new event form" do
+  it 'renders new event form' do
     render
 
-    assert_select "form[action=?][method=?]", events_path, "post" do
+    assert_select "form[action=?][method=?]", events_path, 'post' do
       assert_select "input#event_name[name=?]", "event[name]"
       assert_select "textarea#event_description[name=?]", "event[description]"
       assert_select "input#event_max_participants[name=?]", "event[max_participants]"
