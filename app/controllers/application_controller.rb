6dd7d988d7801aa6b453d4aacadd246eb664b00c
Alexander Riese <alexander.riese@student.hpi.de>
2017-08-09 18:55:27 +0200

    remove fixme

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,10 +14,10 @@ class ApplicationController < ActionController::Base
   end
 
   def index
-    # FIXME: application-side filtering isn't all that great, but
-    #       unless we want to write custom SQL joins (which
-    #       we should if this becomes a perf problem), there is no
-    #       other solution
+    # Application-side filtering isn't all that great, but
+    # unless we want to write custom SQL joins (which
+    # we should if this becomes a perf problem), there is no
+    # other solution
 
     @events = Event.sorted_by_start_date(true)
                    .select { |a| a.start_date > Time.zone.now.yesterday }
