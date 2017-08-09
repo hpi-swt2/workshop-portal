02458dece54bc25584346eccc9e96c1895f77d93
Alexander Riese <alexander.riese@student.hpi.de>
2017-08-09 20:13:39 +0200

    fix missing space after comma

--- app/models/user.rb
+++ app/models/user.rb
@@ -48,7 +48,7 @@ class User < ActiveRecord::Base
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create! do |user|
       user.email = auth.info.email
-      user.password = Devise.friendly_token[0,20]
+      user.password = Devise.friendly_token[0, 20]
       user.role = :pupil
       # user.name = auth.info.first_name + " " + auth.info.last_name
 
