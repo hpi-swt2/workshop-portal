aae7ae5fd8aca06bc1362f4f6d1dc406727fea5a
Bjarne Sievers <jellyfishcoding@live.de>
2017-08-28 19:55:33 +0200

    Fix  rubocop warnings

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -2,8 +2,8 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   skip_before_action :verify_authenticity_token, only: :hpiopenid
 
   def hpiopenid
-    if request.env["omniauth.auth"].info.email.to_s.empty?
-      return redirect_to user_session_path, :alert => I18n.t('users.openid.missing_email')
+    if request.env['omniauth.auth'].info.email.to_s.empty?
+      return redirect_to user_session_path, alert: I18n.t('users.openid.missing_email')
     end
     @user = User.from_omniauth(request.env['omniauth.auth'])
     if @user.persisted?
