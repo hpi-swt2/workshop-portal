87d7ee8e2afce66c8c1501aa19433bc1d0381154
Corinna Jaschek <corinna.jaschek@gmail.com>
2018-04-27 19:08:26 +0200

    start fixing tests, make sure saving works properly

--- spec/factories/requests.rb
+++ spec/factories/requests.rb
@@ -2,9 +2,25 @@
 #
 # Table name: requests
 #
-#  id         :integer          not null, primary key
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
+#  id                     :integer          not null, primary key
+#  created_at             :datetime         not null
+#  updated_at             :datetime         not null
+#  form_of_address        :integer
+#  first_name             :string
+#  last_name              :string
+#  phone_number           :string
+#  school_street          :string
+#  email                  :string
+#  topic_of_workshop      :text
+#  time_period            :text
+#  number_of_participants :integer
+#  knowledge_level        :string
+#  annotations            :text
+#  status                 :integer          default(0)
+#  school_zip_code_city   :string
+#  contact_person         :string
+#  notes                  :text
+#  grade                  :string
 #
 
 FactoryGirl.define do
@@ -13,11 +29,12 @@ FactoryGirl.define do
     first_name "Martina"
     last_name "Mustermann"
     phone_number "0123456789"
-    street "Musterstraße 1"
-    zip_code_city "12345 Musterstadt"
+    school_street "Musterstraße 1"
+    school_zip_code_city "12345 Musterstadt"
     email "mustermann@example.de"
     topic_of_workshop "Hardware-Entwicklung mit einem CAD-System"
     number_of_participants 12
+    grade "8. Klasse"
     status :open
     annotations "Notes about this workshop"
     time_period "Zwischen Ende Januar und Mitte März"
